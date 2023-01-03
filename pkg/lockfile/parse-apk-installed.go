e03ae3d309c40bf7a91534ddfb09f335ab6e1e70
Claudio Maritan <cmaritan@users.noreply.github.com>
2023-01-03 18:24:01 +0100

    APK installed support: refactor to minimize duplicate loops

--- pkg/lockfile/parse-apk-installed.go
+++ pkg/lockfile/parse-apk-installed.go
@@ -9,77 +9,80 @@ import (
 
 const AlpineEcosystem Ecosystem = "Alpine"
 
+func groupApkPackageLines(scanner *bufio.Scanner) [][]string {
+	var groups [][]string
+	var group []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			if len(group) > 0 {
+				groups = append(groups, group)
+			}
+			group = make([]string, 0)
+			continue
+		}
+		group = append(group, line)
+	}
+
+	if len(group) > 0 {
+		groups = append(groups, group)
+	}
+
+	return groups
+}
+
+func parseApkPackageGroup(group []string, pathToLockfile string) (PackageDetails, error) {
+	var pkg PackageDetails = PackageDetails{}
+
+	// File SPECS: https://wiki.alpinelinux.org/wiki/Apk_spec
+	for _, line := range group {
+		switch {
+		case strings.HasPrefix(line, "P:"):
+			pkg.Name = strings.TrimPrefix(line, "P:")
+			pkg.Ecosystem = AlpineEcosystem
+			pkg.CompareAs = AlpineEcosystem
+		case strings.HasPrefix(line, "V:"):
+			pkg.Version = strings.TrimPrefix(line, "V:")
+		case strings.HasPrefix(line, "c:"):
+			pkg.Commit = strings.TrimPrefix(line, "c:")
+		}
+	}
+
+	if pkg.Name == "" {
+		return PackageDetails{}, fmt.Errorf("warning: malformed APK installed file. Found no version number in record. File: %s", pathToLockfile)
+	}
+	if pkg.Version == "" {
+		_, _ = fmt.Fprintf(
+			os.Stderr,
+			"warning: malformed APK installed file. Found no version number in record. File: %s\n",
+			pathToLockfile,
+		)
+	}
+
+	return pkg, nil
+}
+
 func ParseApkInstalled(pathToLockfile string) ([]PackageDetails, error) {
-	var packages []PackageDetails
 
 	file, err := os.Open(pathToLockfile)
 	if err != nil {
-		return packages, fmt.Errorf("could not open %s: %w", pathToLockfile, err)
+		return []PackageDetails{}, fmt.Errorf("could not open %s: %w", pathToLockfile, err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 
-	var curPkg PackageDetails = PackageDetails{}
+	packageGroups := groupApkPackageLines(scanner)
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "" {
-			// First line is empty or multiple empty lines, no current package values
-			if (PackageDetails{}) == curPkg {
-				continue
-			}
-			// Empty line follows a package info block. Append package before going to next one
-			// If Package name is missing, record is invalid then skip
-			if curPkg.Name == "" {
-				_, _ = fmt.Fprintf(
-					os.Stderr,
-					"Warning: malformed APK installed file. Found no package name in record. File: %s\n",
-					pathToLockfile,
-				)
-				curPkg = PackageDetails{}
-				continue
-			}
-			if curPkg.Version == "" {
-				_, _ = fmt.Fprintf(
-					os.Stderr,
-					"Warning: malformed APK installed file. Found no version number in record. File: %s\n",
-					pathToLockfile,
-				)
-			}
-			packages = append(packages, curPkg)
-			curPkg = PackageDetails{}
-			continue
-		}
-		// File SPECS: https://wiki.alpinelinux.org/wiki/Apk_spec
-		if strings.HasPrefix(line, "P:") {
-			curPkg.Name = strings.TrimPrefix(line, "P:")
-			curPkg.Ecosystem = AlpineEcosystem
-			curPkg.CompareAs = AlpineEcosystem
-		} else if strings.HasPrefix(line, "V:") {
-			curPkg.Version = strings.TrimPrefix(line, "V:")
-		} else if strings.HasPrefix(line, "c:") {
-			curPkg.Commit = strings.TrimPrefix(line, "c:")
-		}
+	packages := make([]PackageDetails, 0, len(packageGroups))
 
-	}
-
-	if (PackageDetails{}) != curPkg {
-		if curPkg.Name == "" {
-			_, _ = fmt.Fprintf(
-				os.Stderr,
-				"Warning: malformed APK installed file. Found no package name in record. File: %s\n",
-				pathToLockfile,
-			)
+	for _, group := range packageGroups {
+		if pkg, err := parseApkPackageGroup(group, pathToLockfile); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "%s\n", err)
 		} else {
-			if curPkg.Version == "" {
-				_, _ = fmt.Fprintf(
-					os.Stderr,
-					"Warning: malformed APK installed file. Found no version number in record. File: %s\n",
-					pathToLockfile,
-				)
-			}
-			packages = append(packages, curPkg)
+			packages = append(packages, pkg)
 		}
 	}
 
