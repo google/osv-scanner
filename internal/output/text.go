1a30799ff686261a202c53f57c5132ddffa2cc6a
Michael Kedar <michaelkedar@google.com>
2023-02-01 00:13:12 +0000

    Add truncated details

--- internal/output/text.go
+++ internal/output/text.go
@@ -5,14 +5,48 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
+	"unicode"
 
 	"github.com/google/osv-scanner/internal/osv"
 	"github.com/google/osv-scanner/pkg/models"
 )
 
+// get a description for the vulnerability, truncated to maxLen long.
+// If maxLen < 0, do not truncate.
+func vulnDescription(vuln models.Vulnerability, maxLen int) string {
+	if maxLen > 0 && maxLen < 3 {
+		maxLen = 3
+	}
+	description := vuln.Summary
+	if len(description) == 0 {
+		description = vuln.Details
+	}
+	if len(description) == 0 {
+		if maxLen > 0 && maxLen < len("(no details available)") {
+			return "..."
+		}
+		return "(no details available)"
+	}
+
+	// Only use the first line of a multi-line description.
+	description, _, _ = strings.Cut(description, "\n")
+	if maxLen < 0 || len([]rune(description)) <= maxLen {
+		return description
+	}
+
+	// Find a nice place to truncate the string if too long, ideally not wihtin a word.
+	runes := []rune(description)[:maxLen-3]
+	for i := maxLen - 4; i >= 0; i-- {
+		if unicode.IsSpace(runes[i]) {
+			return string(runes[:i]) + "..."
+		}
+	}
+	return string(runes) + "..."
+}
+
 // PrintTextResults prints the osv scan results as text.
 func PrintTextResults(vulnResult *models.VulnerabilityResults, outputWriter io.Writer) {
-
 	for _, sourceRes := range vulnResult.Results {
 		workingDir, err := os.Getwd()
 		source := sourceRes.Source
@@ -26,53 +60,30 @@ func PrintTextResults(vulnResult *models.VulnerabilityResults, outputWriter io.W
 
 		for _, pkg := range sourceRes.Packages {
 			fmt.Fprintf(outputWriter, "  %s@%s is affected by the following vulnerabilities:\n", pkg.Package.Name, pkg.Package.Version)
-			summaries := make(map[string]string)
+			descriptions := make(map[string]string)
 			for _, vuln := range pkg.Vulnerabilities {
-				summaries[vuln.ID] = vuln.Summary
-			}
-
-			var groupDigits int
-			switch {
-			case len(pkg.Groups) < 10:
-				groupDigits = 1
-			case len(pkg.Groups) < 100:
-				groupDigits = 2
-			default:
-				// There are bigger problems than formatting if a package has more than 999 vulnerabilities
-				groupDigits = 3
+				descriptions[vuln.ID] = vulnDescription(vuln, 80)
 			}
 
 			for i, group := range pkg.Groups {
-				var noSummary []string
-				var withSummary []string
 				pad := 0
 				for _, vuln := range group.IDs {
 					if len(vuln) > pad {
 						pad = len(vuln)
 					}
-					if len(summaries[vuln]) > 0 {
-						withSummary = append(withSummary, vuln)
-					} else {
-						noSummary = append(noSummary, vuln)
-					}
 				}
 
 				pad += len(osv.BaseVulnerabilityURL)
 				vulnStrings := make([]string, 0, len(group.IDs))
-				for _, vuln := range noSummary {
-					vulnStrings = append(vulnStrings, fmt.Sprintf("%s", osv.BaseVulnerabilityURL+vuln))
-				}
-				for _, vuln := range withSummary {
-					vulnStrings = append(vulnStrings, fmt.Sprintf("%-*s - %s", pad, osv.BaseVulnerabilityURL+vuln, summaries[vuln]))
+				for _, vuln := range group.IDs {
+					vulnStrings = append(vulnStrings, fmt.Sprintf("%-*s - %s", pad, osv.BaseVulnerabilityURL+vuln, descriptions[vuln]))
 				}
 
-				fmt.Fprintf(outputWriter, "    %*d. %s\n", groupDigits, i+1, vulnStrings[0])
+				fmt.Fprintf(outputWriter, "    %3d. %s\n", i+1, vulnStrings[0])
 				for _, vulnStr := range vulnStrings[1:] {
-					fmt.Fprintf(outputWriter, "    %*c  %s\n", groupDigits, ' ', vulnStr)
+					fmt.Fprintf(outputWriter, "         %s\n", vulnStr)
 				}
-
 			}
 		}
-
 	}
 }
