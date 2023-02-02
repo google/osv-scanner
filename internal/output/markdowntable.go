701b160cc4eba0e4b30480d11c68d9b0bbfc0e7e
Rex Pan <rexpan@google.com>
2023-02-02 11:52:16 +1100

    Merge markdown and table logic into one function

--- internal/output/markdowntable.go
+++ internal/output/markdowntable.go
@@ -2,12 +2,8 @@ package output
 
 import (
 	"io"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/google/osv-scanner/pkg/models"
-	"github.com/google/osv-scanner/pkg/osv"
 
 	"github.com/jedib0t/go-pretty/v6/table"
 )
@@ -18,43 +14,7 @@ func PrintMarkdownTableResults(vulnResult *models.VulnerabilityResults, outputWr
 	outputTable.SetOutputMirror(outputWriter)
 	outputTable.AppendHeader(table.Row{"OSV URL", "Ecosystem", "Package", "Version", "Source"})
 
-	for _, sourceRes := range vulnResult.Results {
-		for _, pkg := range sourceRes.Packages {
-			workingDir, err := os.Getwd()
-			source := sourceRes.Source
-			if err == nil {
-				sourcePath, err := filepath.Rel(workingDir, source.Path)
-				if err == nil { // Simplify the path if possible
-					source.Path = sourcePath
-				}
-			}
-
-			for _, group := range pkg.Groups {
-				outputRow := table.Row{}
-				shouldMerge := false
-
-				var ids []string
-				var links []string
-
-				for _, vuln := range group.IDs {
-					ids = append(ids, vuln)
-					links = append(links, osv.BaseVulnerabilityURL+vuln)
-				}
-
-				outputRow = append(outputRow, strings.Join(links, " <br>"))
-
-				if pkg.Package.Ecosystem == "GIT" {
-					outputRow = append(outputRow, "GIT", pkg.Package.Version, pkg.Package.Version)
-					shouldMerge = true
-				} else {
-					outputRow = append(outputRow, pkg.Package.Ecosystem, pkg.Package.Name, pkg.Package.Version)
-				}
-
-				outputRow = append(outputRow, source.Path)
-				outputTable.AppendRow(outputRow, table.RowConfig{AutoMerge: shouldMerge})
-			}
-		}
-	}
+	outputTable = tableBuilder(outputTable, vulnResult, false)
 
 	if outputTable.Length() == 0 {
 		return
