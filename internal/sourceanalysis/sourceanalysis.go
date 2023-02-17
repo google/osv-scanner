3a0bc75f4582e50427a35a94a6e22a211a02448d
Rex Pan <rexpan@google.com>
2023-02-17 11:07:54 +1100

    Refactor to remove inter dependencies and make code easier to read.

---
+++ internal/sourceanalysis/sourceanalysis.go
@@ -0,0 +1,53 @@
+package sourceanalysis
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/google/osv-scanner/internal/govulncheckshim"
+	"github.com/google/osv-scanner/internal/output"
+	"github.com/google/osv-scanner/pkg/models"
+	"golang.org/x/exp/slices"
+)
+
+// vulnsFromAllPkgs returns a map of IDs to GroupInfo pointers, and the extracted list of vulnerabilities
+func vulnsFromAllPkgs(pkgs []models.PackageVulns) (map[string]*models.GroupInfo, []models.Vulnerability) {
+	idToGroupMap := map[string]*models.GroupInfo{}
+	vulnList := []models.Vulnerability{}
+	for _, pv := range pkgs {
+		for _, vuln := range pv.Vulnerabilities {
+			groupIdx := slices.IndexFunc(pv.Groups, func(g models.GroupInfo) bool {
+				return slices.Contains(g.IDs, vuln.ID)
+			})
+
+			idToGroupMap[vuln.ID] = &pv.Groups[groupIdx]
+			vulnList = append(vulnList, vuln)
+		}
+	}
+
+	return idToGroupMap, vulnList
+}
+
+func DoSourceAnalysis(r *output.Reporter, source models.SourceInfo, pkgs []models.PackageVulns) {
+	if source.Type == "lockfile" && filepath.Base(source.Path) == "go.mod" {
+		idToGroupMap, allVulns := vulnsFromAllPkgs(pkgs)
+		res, err := govulncheckshim.RunGoVulnCheck(filepath.Dir(source.Path), allVulns)
+		if err != nil {
+			// TODO: Better method to identify the type of error and give advice specific to the error
+			r.PrintError(
+				fmt.Sprintf("Failed to run code analysis (govulncheck) on '%s' because %s\n"+
+					"(the Go toolchain is required)\n", source.Path, err.Error()))
+
+			return
+		}
+		for _, v := range res.Vulns {
+			analysis := &idToGroupMap[v.OSV.ID].ExperimentalAnalysis
+			if *analysis == nil {
+				*analysis = make(map[string]models.AnalysisInfo)
+			}
+			(*analysis)[v.OSV.ID] = models.AnalysisInfo{
+				Called: v.IsCalled(),
+			}
+		}
+	}
+}
