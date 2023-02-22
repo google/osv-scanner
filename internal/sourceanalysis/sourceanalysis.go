09645709ffc69f351a9356032b39dbd218cf9259
Rex Pan <rexpan@google.com>
2023-02-22 12:36:27 +1100

    Update to resolve multiple packages issue

--- internal/sourceanalysis/sourceanalysis.go
+++ internal/sourceanalysis/sourceanalysis.go
@@ -4,37 +4,58 @@ import (
 	"fmt"
 	"path/filepath"
 
+	"golang.org/x/exp/slices"
+
 	"github.com/google/osv-scanner/internal/govulncheckshim"
 	"github.com/google/osv-scanner/internal/output"
 	"github.com/google/osv-scanner/pkg/models"
-	"golang.org/x/exp/slices"
 )
 
+type MatchedVulnerability struct {
+	vuln  models.Vulnerability
+	group *models.GroupInfo
+	pkg   models.PackageInfo
+}
+
 // vulnsFromAllPkgs returns a map of IDs to GroupInfo pointers, and the extracted list of vulnerabilities
-func vulnsFromAllPkgs(pkgs []models.PackageVulns) (map[string]*models.GroupInfo, []models.Vulnerability) {
-	idToGroupMap := map[string]*models.GroupInfo{}
-	vulnList := []models.Vulnerability{}
+func vulnsFromAllPkgs(pkgs []models.PackageVulns) (map[string]map[string]MatchedVulnerability, []models.Vulnerability) {
+	idMatchedVulnMap := map[string]map[string]MatchedVulnerability{}
+	flatVulns := map[string]models.Vulnerability{}
 	for _, pv := range pkgs {
 		for _, vuln := range pv.Vulnerabilities {
 			groupIdx := slices.IndexFunc(pv.Groups, func(g models.GroupInfo) bool {
 				return slices.Contains(g.IDs, vuln.ID)
 			})
 
-			idToGroupMap[vuln.ID] = &pv.Groups[groupIdx]
-			vulnList = append(vulnList, vuln)
+			if idMatchedVulnMap[pv.Package.Name] == nil {
+				idMatchedVulnMap[pv.Package.Name] = make(map[string]MatchedVulnerability)
+			}
+
+			idMatchedVulnMap[pv.Package.Name][vuln.ID] = MatchedVulnerability{
+				group: &pv.Groups[groupIdx],
+				vuln:  vuln,
+				pkg:   pv.Package,
+			}
+
+			flatVulns[vuln.ID] = vuln
 		}
 	}
 
-	return idToGroupMap, vulnList
+	vulnList := []models.Vulnerability{}
+	for _, v := range flatVulns {
+		vulnList = append(vulnList, v)
+	}
+
+	return idMatchedVulnMap, vulnList
 }
 
 // Run runs the language specific analyzers on the code given packages and source info
 func Run(r *output.Reporter, source models.SourceInfo, pkgs []models.PackageVulns) {
-	idToGroupMap, allVulns := vulnsFromAllPkgs(pkgs)
+	idMatchedVulnMap, vulnsSlice := vulnsFromAllPkgs(pkgs)
 
 	// GoVulnCheck
 	if source.Type == "lockfile" && filepath.Base(source.Path) == "go.mod" {
-		res, err := govulncheckshim.RunGoVulnCheck(filepath.Dir(source.Path), allVulns)
+		res, err := govulncheckshim.RunGoVulnCheck(filepath.Dir(source.Path), vulnsSlice)
 		if err != nil {
 			// TODO: Better method to identify the type of error and give advice specific to the error
 			r.PrintError(
@@ -45,13 +66,19 @@ func Run(r *output.Reporter, source models.SourceInfo, pkgs []models.PackageVuln
 		}
 		// Add analysis information back into package list
 		for _, v := range res.Vulns {
-			analysis := &idToGroupMap[v.OSV.ID].ExperimentalAnalysis
-			if *analysis == nil {
-				*analysis = make(map[string]models.AnalysisInfo)
+			for _, m := range v.Modules {
+				if idMatchedVulnMap[m.Path] == nil {
+					continue
+				}
+				analysis := &idMatchedVulnMap[m.Path][v.OSV.ID].group.ExperimentalAnalysis
+				if *analysis == nil {
+					*analysis = make(map[string]models.AnalysisInfo)
+				}
+				(*analysis)[v.OSV.ID] = models.AnalysisInfo{
+					Called: v.IsCalled(),
+				}
 			}
-			(*analysis)[v.OSV.ID] = models.AnalysisInfo{
-				Called: v.IsCalled(),
-			}
+
 		}
 	}
 }
