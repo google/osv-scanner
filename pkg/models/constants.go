3f3623bfddfec6a7e434e781a77317fcb7087c37
Caleb Brown <calebbrown@google.com>
2023-03-23 12:16:58 +1100

    Fix constant names.
    Signed-off-by: Caleb Brown <calebbrown@google.com>

--- pkg/models/constants.go
+++ pkg/models/constants.go
@@ -3,63 +3,63 @@ package models
 type Ecosystem string
 
 const (
-	ECOSYSTEM_GO             Ecosystem = "Go"
-	ECOSYSTEM_NPM            Ecosystem = "npm"
-	ECOSYSTEM_OSS_FUZZ       Ecosystem = "OSS-Fuzz"
-	ECOSYSTEM_PYPI           Ecosystem = "PyPI"
-	ECOSYSTEM_RUBYGEMS       Ecosystem = "RubyGems"
-	ECOSYSTEM_CRATES_IO      Ecosystem = "crates.io"
-	ECOSYSTEM_PACKAGIST      Ecosystem = "Packagist"
-	ECOSYSTEM_MAVEN          Ecosystem = "Maven"
-	ECOSYSTEM_NUGET          Ecosystem = "NuGet"
-	ECOSYSTEM_LINUX          Ecosystem = "Linux"
-	ECOSYSTEM_DEBIAN         Ecosystem = "Debian"
-	ECOSYSTEM_ALPINE         Ecosystem = "Alpine"
-	ECOSYSTEM_HEX            Ecosystem = "Hex"
-	ECOSYSTEM_ANDROID        Ecosystem = "Android"
-	ECOSYSTEM_GITHUB_ACTIONS Ecosystem = "GitHub Actions"
-	ECOSYSTEM_PUB            Ecosystem = "Pub"
-	ECOSYSTEM_CONANCENTER    Ecosystem = "ConanCenter"
+	EcosystemGo            Ecosystem = "Go"
+	EcosystemNPM           Ecosystem = "npm"
+	EcosystemOSSFuzz       Ecosystem = "OSS-Fuzz"
+	EcosystemPyPI          Ecosystem = "PyPI"
+	EcosystemRubyGems      Ecosystem = "RubyGems"
+	EcosystemCratesIO      Ecosystem = "crates.io"
+	EcosystemPackagist     Ecosystem = "Packagist"
+	EcosystemMaven         Ecosystem = "Maven"
+	EcosystemNuGet         Ecosystem = "NuGet"
+	EcosystemLinux         Ecosystem = "Linux"
+	EcosystemDebian        Ecosystem = "Debian"
+	EcosystemAlpine        Ecosystem = "Alpine"
+	EcosystemHex           Ecosystem = "Hex"
+	EcosystemAndroid       Ecosystem = "Android"
+	EcosystemGitHubActions Ecosystem = "GitHub Actions"
+	EcosystemPub           Ecosystem = "Pub"
+	EcosystemConanCenter   Ecosystem = "ConanCenter"
 )
 
 type SeverityType string
 
 const (
-	SEVERITY_CVSS_V2 SeverityType = "CVSS_V2"
-	SEVERITY_CVSS_V3 SeverityType = "CVSS_V3"
+	SeverityCVSSV2 SeverityType = "CVSS_V2"
+	SeverityCVSSV3 SeverityType = "CVSS_V3"
 )
 
 type RangeType string
 
 const (
-	RANGE_SEMVER    RangeType = "SEMVER"
-	RANGE_ECOSYSTEM RangeType = "ECOSYSTEM"
-	RANGE_GIT       RangeType = "GIT"
+	RangeSemVer    RangeType = "SEMVER"
+	RangeEcosystem RangeType = "ECOSYSTEM"
+	RangeGit       RangeType = "GIT"
 )
 
 type ReferenceType string
 
 const (
-	REFERENCE_ADVISORY ReferenceType = "ADVISORY"
-	REFERENCE_ARTICLE  ReferenceType = "ARTICLE"
-	REFERENCE_REPORT   ReferenceType = "REPORT"
-	REFERENCE_FIX      ReferenceType = "FIX"
-	REFERENCE_PACKAGE  ReferenceType = "PACKAGE"
-	REFERENCE_EVIDENCE ReferenceType = "EVIDENCE"
-	REFERENCE_WEB      ReferenceType = "WEB"
+	ReferenceAdvisory ReferenceType = "ADVISORY"
+	ReferenceArticle  ReferenceType = "ARTICLE"
+	ReferenceReport   ReferenceType = "REPORT"
+	ReferenceFix      ReferenceType = "FIX"
+	ReferencePackage  ReferenceType = "PACKAGE"
+	ReferenceEvidence ReferenceType = "EVIDENCE"
+	ReferenceWeb      ReferenceType = "WEB"
 )
 
 type CreditType string
 
 const (
-	CREDIT_FINDER                CreditType = "FINDER"
-	CREDIT_REPORTER              CreditType = "REPORTER"
-	CREDIT_ANALYST               CreditType = "ANALYST"
-	CREDIT_COORDINATOR           CreditType = "COORDINATOR"
-	CREDIT_REMEDIATION_DEVELOPER CreditType = "REMEDIATION_DEVELOPER"
-	CREDIT_REMEDIATION_REVIEWER  CreditType = "REMEDIATION_REVIEWER"
-	CREDIT_REMEDIATION_VERIFIER  CreditType = "REMEDIATION_VERIFIER"
-	CREDIT_TOOL                  CreditType = "TOOL"
-	CREDIT_SPONSOR               CreditType = "SPONSOR"
-	CREDIT_OTHER                 CreditType = "OTHER"
+	CreditFinder               CreditType = "FINDER"
+	CreditReporter             CreditType = "REPORTER"
+	CreditAnalyst              CreditType = "ANALYST"
+	CreditCoordinator          CreditType = "COORDINATOR"
+	CreditRemediationDeveloper CreditType = "REMEDIATION_DEVELOPER"
+	CreditRemediationReviewer  CreditType = "REMEDIATION_REVIEWER"
+	CreditRemediationVerifier  CreditType = "REMEDIATION_VERIFIER"
+	CreditTool                 CreditType = "TOOL"
+	CreditSponsor              CreditType = "SPONSOR"
+	CreditOther                CreditType = "OTHER"
 )
