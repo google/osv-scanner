add35fdb310f60e589130af80c00fa72b73a080f
Rex Pan <rexpan@google.com>
2023-07-06 11:07:12 +1000

    Fix sarif test

--- internal/output/sarif_test.go
+++ internal/output/sarif_test.go
@@ -14,15 +14,16 @@ func TestGroupFixedVersions(t *testing.T) {
 		flattened []models.VulnerabilityFlattened
 	}
 	tests := []struct {
-		name string
-		args args
-		want map[string][]string
+		name     string
+		args     args
+		wantPath string
 	}{
 		{
 			name: "",
 			args: args{
 				flattened: testutility.LoadJSONFixture[[]models.VulnerabilityFlattened](t, "fixtures/flattened_vulns.json"),
 			},
+			wantPath: "fixtures/group_fixed_version_output.json",
 		},
 	}
 	for _, tt := range tests {
@@ -30,7 +31,7 @@ func TestGroupFixedVersions(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			got := GroupFixedVersions(tt.args.flattened)
-			testutility.AssertMatchFixtureJSON(t, "fixtures/group_fixed_version_output.json", got)
+			testutility.AssertMatchFixtureJSON(t, tt.wantPath, got)
 		})
 	}
 }
