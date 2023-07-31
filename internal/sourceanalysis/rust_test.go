75d71be206fcfc200ddf5cd61c702b7784952996
Rex P <rexpan@google.com>
2023-07-31 17:01:28 +1000

    Optimise test

--- internal/sourceanalysis/rust_test.go
+++ internal/sourceanalysis/rust_test.go
@@ -9,23 +9,28 @@ import (
 )
 
 func Test_extractRlibArchive(t *testing.T) {
+	t.Parallel()
 	entries, err := os.ReadDir("fixtures-rust/archives")
 	if err != nil {
 		t.Error(err)
 	}
 	for _, file := range entries {
-		buf, err := extractRlibArchive(filepath.Join("fixtures-rust/archives", file.Name()))
-		if err != nil {
-			t.Error(err)
-		}
+		filename := file.Name()
+		t.Run("Extract Rlib "+filename, func(t *testing.T) {
+			t.Parallel()
+			buf, err := extractRlibArchive(filepath.Join("fixtures-rust/archives", file.Name()))
+			if err != nil {
+				t.Error(err)
+			}
 
-		expectedFileName := strings.Replace(file.Name(), ".rlib", ".o", 1)
-		expectedBuf, err := os.ReadFile(filepath.Join("fixtures-rust/objs", expectedFileName))
-		if err != nil {
-			t.Error(err)
-		}
-		if !bytes.Equal(buf.Bytes(), expectedBuf) {
-			t.Fatalf("Extracted not identical to expected: %s", filepath.Join("fixtures-rust/archives", file.Name()))
-		}
+			expectedFileName := strings.Replace(file.Name(), ".rlib", ".o", 1)
+			expectedBuf, err := os.ReadFile(filepath.Join("fixtures-rust/objs", expectedFileName))
+			if err != nil {
+				t.Error(err)
+			}
+			if !bytes.Equal(buf.Bytes(), expectedBuf) {
+				t.Fatalf("Extracted not identical to expected: %s", filepath.Join("fixtures-rust/archives", file.Name()))
+			}
+		})
 	}
 }
