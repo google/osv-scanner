6e8a392f238709989a97d3d58e3774eb8769b174
Rex P <rexpan@google.com>
2023-08-30 13:46:05 +1000

    Revert breaking type changes

--- internal/local/check.go
+++ internal/local/check.go
@@ -38,8 +38,8 @@ func toPackageDetails(query *osv.Query) (lockfile.PackageDetails, error) {
 		Name:      query.Package.Name,
 		Version:   query.Version,
 		Commit:    query.Commit,
-		Ecosystem: query.Package.Ecosystem,
-		CompareAs: query.Package.Ecosystem,
+		Ecosystem: lockfile.Ecosystem(query.Package.Ecosystem),
+		CompareAs: lockfile.Ecosystem(query.Package.Ecosystem),
 	}, nil
 }
 
--- pkg/osv/osv.go
+++ pkg/osv/osv.go
@@ -32,9 +32,9 @@ var RequestUserAgent = ""
 
 // Package represents a package identifier for OSV.
 type Package struct {
-	PURL      string             `json:"purl,omitempty"`
-	Name      string             `json:"name,omitempty"`
-	Ecosystem lockfile.Ecosystem `json:"ecosystem,omitempty"`
+	PURL      string `json:"purl,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Ecosystem string `json:"ecosystem,omitempty"`
 }
 
 // Query represents a query to OSV.
@@ -57,7 +57,7 @@ type MinimalVulnerability struct {
 
 // Response represents a full response from OSV.
 type Response struct {
-	Vulns models.Vulnerabilities `json:"vulns"`
+	Vulns []models.Vulnerability `json:"vulns"`
 }
 
 // MinimalResponse represents an unhydrated response from OSV.
@@ -98,7 +98,7 @@ func MakePkgRequest(pkgDetails lockfile.PackageDetails) *Query {
 		// Commit:  pkgDetails.Commit,
 		Package: Package{
 			Name:      pkgDetails.Name,
-			Ecosystem: pkgDetails.Ecosystem,
+			Ecosystem: string(pkgDetails.Ecosystem),
 		},
 	}
 }
