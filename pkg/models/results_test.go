4a9a5bbcacbe0f0463ea1eebb3a9d7cb3a9ba8eb
Josie Anugerah <josieang@google.com>
2023-08-30 21:02:08 +1000

    Add tests for flattening licenses

--- pkg/models/results_test.go
+++ pkg/models/results_test.go
@@ -32,6 +32,7 @@ func TestFlatten(t *testing.T) {
 				},
 			},
 		},
+		Licenses: []License{License("MIT")},
 	}
 	source := PackageSource{Source: SourceInfo{Path: "package"}, Packages: []PackageVulns{pkg}}
 	vulns = VulnerabilityResults{Results: []PackageSource{source}}
@@ -47,4 +48,68 @@ func TestFlatten(t *testing.T) {
 	if diff := cmp.Diff(flattened, expectedFlattened); diff != "" {
 		t.Errorf("Flatten() returned unexpected result (-got +want):\n%s", diff)
 	}
+
+	// Test case 3: When there are no vulnerabilities and but license violations
+	group = GroupInfo{IDs: []string{"CVE-2021-1234"}}
+	pkg = PackageVulns{
+		Package:           PackageInfo{Name: "package"},
+		Groups:            []GroupInfo{group},
+		Licenses:          []License{License("MIT")},
+		LicenseViolations: []License{License("MIT")},
+	}
+	source = PackageSource{Source: SourceInfo{Path: "package"}, Packages: []PackageVulns{pkg}}
+	vulns = VulnerabilityResults{Results: []PackageSource{source}}
+	expectedFlattened = []VulnerabilityFlattened{
+		{
+			Source:            source.Source,
+			Package:           pkg.Package,
+			Licenses:          []License{License("MIT")},
+			LicenseViolations: []License{License("MIT")},
+		},
+	}
+	flattened = vulns.Flatten()
+	if diff := cmp.Diff(flattened, expectedFlattened); diff != "" {
+		t.Errorf("Flatten() returned unexpected result (-got +want):\n%s", diff)
+	}
+
+	// Test case 4: When there are vulnerabilities and license violations
+	group = GroupInfo{IDs: []string{"CVE-2021-1234"}}
+	pkg = PackageVulns{
+		Package: PackageInfo{Name: "package"},
+		Groups:  []GroupInfo{group},
+		Vulnerabilities: []Vulnerability{
+			{
+				ID: "CVE-2021-1234",
+				Severity: []Severity{
+					{
+						Type:  SeverityType("high"),
+						Score: "1",
+					},
+				},
+			},
+		},
+		Licenses:          []License{License("MIT")},
+		LicenseViolations: []License{License("MIT")},
+	}
+	source = PackageSource{Source: SourceInfo{Path: "package"}, Packages: []PackageVulns{pkg}}
+	vulns = VulnerabilityResults{Results: []PackageSource{source}}
+	expectedFlattened = []VulnerabilityFlattened{
+		{
+			Source:        source.Source,
+			Package:       pkg.Package,
+			Vulnerability: pkg.Vulnerabilities[0],
+			GroupInfo:     group,
+		},
+		{
+			Source:            source.Source,
+			Package:           pkg.Package,
+			Licenses:          []License{License("MIT")},
+			LicenseViolations: []License{License("MIT")},
+		},
+	}
+	flattened = vulns.Flatten()
+	if diff := cmp.Diff(flattened, expectedFlattened); diff != "" {
+		t.Errorf("Flatten() returned unexpected result (-got +want):\n%s", diff)
+	}
+
 }
