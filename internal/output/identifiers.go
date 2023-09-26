3791c92a7ec9c1898a6369701d47b773371f2c53
Oliver Chang <oliverchang@users.noreply.github.com>
2023-09-26 12:39:22 +1000

    SARIF output fixes. (#547)
    - Use relative path for URIs.
    - Fix description/summary stability (due to map insertion order not
    being preserved), and sort by `<ECOSYSTEM> < <GHSA> < <CVE>` for
    extracting descriptions.
    - Remove an extraneous period.
    
    
    ![image](https://github.com/google/osv-scanner/assets/759062/74d12906-d790-4d03-9830-c47ebc5106f8)

--- internal/output/identifiers.go
+++ internal/output/identifiers.go
@@ -16,10 +16,29 @@ func prefixOrder(prefix string) int {
 	return 1
 }
 
+func prefixOrderForDescription(prefix string) int {
+	if prefix == "CVE" {
+		return 0
+	} else if prefix == "GHSA" {
+		return 1
+	}
+
+	return 2
+}
+
 // idSortFunc sorts IDs ascending by CVE < [ECO-SPECIFIC] < GHSA
 func idSortFunc(a, b string) int {
-	prefixAOrd := prefixOrder(strings.Split(a, "-")[0])
-	prefixBOrd := prefixOrder(strings.Split(b, "-")[0])
+	return idSort(a, b, prefixOrder)
+}
+
+// idSortFuncForDescription sorts ID ascending by [ECO-SPECIFIC] < GHSA < CVE
+func idSortFuncForDescription(a, b string) int {
+	return idSort(a, b, prefixOrderForDescription)
+}
+
+func idSort(a, b string, prefixOrd func(string) int) int {
+	prefixAOrd := prefixOrd(strings.Split(a, "-")[0])
+	prefixBOrd := prefixOrd(strings.Split(b, "-")[0])
 
 	if prefixAOrd > prefixBOrd {
 		return -1
