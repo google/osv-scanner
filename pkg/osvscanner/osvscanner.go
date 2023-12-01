8180ef4388f2249f4ab7920d8e6ec9f8b6afd508
Rex P <rexpan@google.com>
2023-12-01 12:00:06 +1100

    Fix vendored deps for windows

--- pkg/osvscanner/osvscanner.go
+++ pkg/osvscanner/osvscanner.go
@@ -1,11 +1,9 @@
 package osvscanner
 
 import (
-	"bufio"
-	"crypto/md5" //nolint:gosec
+	"bufio" //nolint:gosec
 	"errors"
 	"fmt"
-	"io/fs"
 	"os"
 	"os/exec"
 	"path"
@@ -70,22 +68,6 @@ var VulnerabilitiesFoundErr = errors.New("vulnerabilities found")
 //nolint:errname,stylecheck // Would require version bump to change
 var OnlyUncalledVulnerabilitiesFoundErr = errors.New("only uncalled vulnerabilities found")
 
-var (
-	vendoredLibNames = map[string]struct{}{
-		"3rdparty":    {},
-		"dep":         {},
-		"deps":        {},
-		"thirdparty":  {},
-		"third-party": {},
-		"third_party": {},
-		"libs":        {},
-		"external":    {},
-		"externals":   {},
-		"vendor":      {},
-		"vendored":    {},
-	}
-)
-
 const (
 	// This value may need to be tweaked, or be provided as a configurable flag.
 	determineVersionThreshold = 0.15
@@ -141,6 +123,7 @@ func scanDir(r reporter.Reporter, dir string, skipGit bool, recursive bool, useG
 			}
 		}
 
+		// Scan git commit hash
 		if !skipGit && info.IsDir() && info.Name() == ".git" {
 			pkgs, err := scanGit(r, filepath.Dir(path)+"/")
 			if err != nil {
@@ -152,6 +135,7 @@ func scanDir(r reporter.Reporter, dir string, skipGit bool, recursive bool, useG
 			return filepath.SkipDir
 		}
 
+		// Scan lockfiles
 		if !info.IsDir() {
 			if extractor, _ := lockfile.FindExtractor(path, ""); extractor != nil {
 				pkgs, err := scanLockfile(r, path, "")
@@ -167,6 +151,7 @@ func scanDir(r reporter.Reporter, dir string, skipGit bool, recursive bool, useG
 			scannedPackages = append(scannedPackages, pkgs...)
 		}
 
+		// Scan vendored directories
 		if info.IsDir() && !compareOffline {
 			if _, ok := vendoredLibNames[strings.ToLower(filepath.Base(path))]; ok {
 				pkgs, err := scanDirWithVendoredLibs(r, path)
@@ -223,94 +208,6 @@ func parseGitIgnores(path string) (*gitIgnoreMatcher, error) {
 	}
 
 	return &gitIgnoreMatcher{matcher: matcher, repoPath: repopath}, nil
-}
-
-func queryDetermineVersions(repoDir string) (*osv.DetermineVersionResponse, error) {
-	fileExts := []string{
-		".hpp",
-		".h",
-		".hh",
-		".cc",
-		".c",
-		".cpp",
-	}
-
-	var hashes []osv.DetermineVersionHash
-	if err := filepath.Walk(repoDir, func(p string, info fs.FileInfo, err error) error {
-		if info.IsDir() {
-			if _, err := os.Stat(filepath.Join(p, ".git")); err == nil {
-				// Found a git repo, stop here as otherwise we may get duplicated
-				// results with our regular git commit scanning.
-				return filepath.SkipDir
-			}
-			if _, ok := vendoredLibNames[strings.ToLower(info.Name())]; ok {
-				// Ignore nested vendored libraries, as they can cause bad matches.
-				return filepath.SkipDir
-			}
-
-			return nil
-		}
-		for _, ext := range fileExts {
-			if filepath.Ext(p) == ext {
-				buf, err := os.ReadFile(p)
-				if err != nil {
-					return err
-				}
-				hash := md5.Sum(buf) //nolint:gosec
-				hashes = append(hashes, osv.DetermineVersionHash{
-					Path: strings.ReplaceAll(p, repoDir, ""),
-					Hash: hash[:],
-				})
-				if len(hashes) > maxDetermineVersionFiles {
-					return errors.New("too many files to hash")
-				}
-			}
-		}
-
-		return nil
-	}); err != nil {
-		return nil, fmt.Errorf("failed during hashing: %w", err)
-	}
-
-	result, err := osv.MakeDetermineVersionRequest(filepath.Base(repoDir), hashes)
-	if err != nil {
-		return nil, fmt.Errorf("failed to determine versions: %w", err)
-	}
-
-	return result, nil
-}
-
-func scanDirWithVendoredLibs(r reporter.Reporter, path string) ([]scannedPackage, error) {
-	r.PrintText(fmt.Sprintf("Scanning directory for vendored libs: %s\n", path))
-	entries, err := os.ReadDir(path)
-	if err != nil {
-		return nil, err
-	}
-
-	var packages []scannedPackage
-	for _, entry := range entries {
-		if !entry.IsDir() {
-			continue
-		}
-
-		libPath := filepath.Join(path, entry.Name())
-
-		r.PrintText(fmt.Sprintf("Scanning potential vendored dir: %s\n", libPath))
-		// TODO: make this a goroutine to parallelise this operation
-		results, err := queryDetermineVersions(libPath)
-		if err != nil {
-			r.PrintText(fmt.Sprintf("Error scanning sub-directory '%s' with error: %v", libPath, err))
-			continue
-		}
-
-		if len(results.Matches) > 0 && results.Matches[0].Score > determineVersionThreshold {
-			match := results.Matches[0]
-			r.PrintText(fmt.Sprintf("Identified %s as %s at %s.\n", libPath, match.RepoInfo.Address, match.RepoInfo.Commit))
-			packages = append(packages, createCommitQueryPackage(match.RepoInfo.Commit, libPath))
-		}
-	}
-
-	return packages, nil
 }
 
 // gitIgnoreMatcher.match will return true if the file/directory matches a gitignore entry
---
+++ pkg/osvscanner/vendored_libs.go
@@ -0,0 +1,128 @@
+package osvscanner
+
+import (
+	"bytes"
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/google/osv-scanner/pkg/osv"
+	"github.com/google/osv-scanner/pkg/reporter"
+)
+
+var (
+	vendoredLibNames = map[string]struct{}{
+		"3rdparty":    {},
+		"dep":         {},
+		"deps":        {},
+		"thirdparty":  {},
+		"third-party": {},
+		"third_party": {},
+		"libs":        {},
+		"external":    {},
+		"externals":   {},
+		"vendor":      {},
+		"vendored":    {},
+	}
+)
+
+func scanDirWithVendoredLibs(r reporter.Reporter, path string) ([]scannedPackage, error) {
+	r.PrintText(fmt.Sprintf("Scanning directory for vendored libs: %s\n", path))
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var packages []scannedPackage
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			continue
+		}
+
+		libPath := filepath.Join(path, entry.Name())
+
+		r.PrintText(fmt.Sprintf("Scanning potential vendored dir: %s\n", libPath))
+		// TODO: make this a goroutine to parallelize this operation
+		results, err := queryDetermineVersions(libPath)
+		if err != nil {
+			r.PrintText(fmt.Sprintf("Error scanning sub-directory '%s' with error: %v", libPath, err))
+			continue
+		}
+
+		if len(results.Matches) > 0 && results.Matches[0].Score > determineVersionThreshold {
+			match := results.Matches[0]
+			r.PrintText(fmt.Sprintf("Identified %s as %s at %s.\n", libPath, match.RepoInfo.Address, match.RepoInfo.Commit))
+			packages = append(packages, createCommitQueryPackage(match.RepoInfo.Commit, libPath))
+		}
+	}
+
+	return packages, nil
+}
+
+func queryDetermineVersions(repoDir string) (*osv.DetermineVersionResponse, error) {
+	fileExts := []string{
+		".hpp",
+		".h",
+		".hh",
+		".cc",
+		".c",
+		".cpp",
+	}
+
+	var hashes []osv.DetermineVersionHash
+	if err := filepath.Walk(repoDir, func(p string, info fs.FileInfo, err error) error {
+		if info.IsDir() {
+			if _, err := os.Stat(filepath.Join(p, ".git")); err == nil {
+				// Found a git repo, stop here as otherwise we may get duplicated
+				// results with our regular git commit scanning.
+				return filepath.SkipDir
+			}
+			if _, ok := vendoredLibNames[strings.ToLower(info.Name())]; ok {
+				// Ignore nested vendored libraries, as they can cause bad matches.
+				return filepath.SkipDir
+			}
+
+			return nil
+		}
+
+		windowsEnding := []byte("\r\n")
+		unixEnding := []byte("\n")
+
+		for _, ext := range fileExts {
+			if filepath.Ext(p) != ext {
+				continue
+			}
+
+			buf, err := os.ReadFile(p)
+			if err != nil {
+				return err
+			}
+
+			buf = bytes.ReplaceAll(buf, windowsEnding, unixEnding)
+
+			hash := md5.Sum(buf) //nolint:gosec
+			hashes = append(hashes, osv.DetermineVersionHash{
+				Path: strings.ReplaceAll(p, repoDir, ""),
+				Hash: hash[:],
+			})
+			if len(hashes) > maxDetermineVersionFiles {
+				return errors.New("too many files to hash")
+			}
+		}
+
+		return nil
+	}); err != nil {
+		return nil, fmt.Errorf("failed during hashing: %w", err)
+	}
+
+	result, err := osv.MakeDetermineVersionRequest(filepath.Base(repoDir), hashes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to determine versions: %w", err)
+	}
+
+	return result, nil
+}
