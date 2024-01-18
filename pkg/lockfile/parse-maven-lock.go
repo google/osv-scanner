45f6e3d20e9cf40e43b537b8e1b7e9341b8181db
Marc Wieser <marc.wieser@datadoghq.com>
2024-01-18 14:01:29 +0100

    :construction: Try to put in abs for windows failure in file opening

--- pkg/lockfile/parse-maven-lock.go
+++ pkg/lockfile/parse-maven-lock.go
@@ -216,7 +216,7 @@ func (e MavenLockExtractor) decodeMavenFile(f DepFile, depth int) (*MavenLockFil
 		// It means we only have a path, we should append the default pom.xml
 		parentRelativePath = path.Join(parentRelativePath, "pom.xml")
 	}
-	parentPath := filepath.FromSlash(path.Join(path.Dir(f.Path()), parentRelativePath))
+	parentPath, _ := filepath.Abs(filepath.FromSlash(path.Join(path.Dir(f.Path()), parentRelativePath)))
 	fmt.Printf("Opening parent file %v\n", parentPath)
 	parentFile, err := OpenLocalDepFile(parentPath)
 	if err != nil {
