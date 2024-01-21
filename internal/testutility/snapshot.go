367c846f54460abe4c12b191fc2bd5a81f706b1a
Gareth Jones <Jones258@Gmail.com>
2024-01-22 11:50:22 +1300

    test: use snapshot-based testing (#717)
    This replaces all our bulky string output comparison tests with nice
    automated snapshot tests - sample output:
    
    <img width="669" alt="image"
    src="https://github.com/google/osv-scanner/assets/3151613/9bbd3f4b-550d-4ed2-a7e9-c68a2f29ff9e">
    
    Snapshots are generated automatically if they don't exist already when
    the test suite is run - otherwise they can be regenerated by setting
    `UPDATE_SNAPS=true`:
    
    ```
    UPDATE_SNAPS=true scripts/run_scripts.sh
    ```
    
    I will update the contributing documentation at some point, but the code
    should be good to go - I've tried not to go too overboard with helper
    methods 😅
    
    ---------
    
    Co-authored-by: Rex P <106129829+another-rex@users.noreply.github.com>

---
+++ internal/testutility/snapshot.go
@@ -0,0 +1,46 @@
+package testutility
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/gkampitakis/go-snaps/snaps"
+)
+
+type Snapshot struct {
+	windowsReplacements map[string]string
+}
+
+// NewSnapshot creates a snapshot that can be passed around within tests
+func NewSnapshot() Snapshot {
+	return Snapshot{windowsReplacements: map[string]string{}}
+}
+
+// WithWindowsReplacements adds a map of strings with values that they should be
+// replaced within before comparing the snapshot when running on Windows
+func (s Snapshot) WithWindowsReplacements(replacements map[string]string) Snapshot {
+	s.windowsReplacements = replacements
+
+	return s
+}
+
+// MatchJSON asserts the existing snapshot matches what was gotten in the test,
+// after being marshalled as JSON
+func (s Snapshot) MatchJSON(t *testing.T, got any) {
+	t.Helper()
+
+	j, err := json.MarshalIndent(got, "", "  ")
+
+	if err != nil {
+		t.Fatalf("Failed to marshal JSON: %s", err)
+	}
+
+	s.MatchText(t, string(j))
+}
+
+// MatchText asserts the existing snapshot matches what was gotten in the test
+func (s Snapshot) MatchText(t *testing.T, got string) {
+	t.Helper()
+
+	snaps.MatchSnapshot(t, applyWindowsReplacements(got, s.windowsReplacements))
+}
