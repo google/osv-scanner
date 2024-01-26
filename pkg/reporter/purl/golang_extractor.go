e9587e359d50f45285557687335c94fd259ab1b1
Marc Wieser <marc.wieser@datadoghq.com>
2024-01-26 11:35:10 +0100

    :bug: Fixing the golang extractor to filter empty packages

--- pkg/reporter/purl/golang_extractor.go
+++ pkg/reporter/purl/golang_extractor.go
@@ -9,8 +9,9 @@ import (
 
 func ExtractPURLFromGolang(packageInfo models.PackageInfo) (namespace string, name string, ok bool) {
 	nameParts := strings.Split(packageInfo.Name, "/")
-	if len(nameParts) == 0 {
+	if len(nameParts) == 0 || len(packageInfo.Name) == 0 {
 		log.Printf("invalid golang package_name=%s", packageInfo.Name)
+		ok = false
 		return
 	}
 	ok = true
