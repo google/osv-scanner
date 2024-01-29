4528ca239b34bac4264be4eaac9b069168ee18fa
Michael Kedar <michaelkedar@google.com>
2024-01-29 12:54:44 +1100

    Guided Remediation: Add computation for all relaxation patches (#766)
    Following on from #765, adds `ComputeRelaxPatches` for generating the
    possible remediation options after a relock.
    Also added a new(ish) cache for OSV API requests, which speeds up the
    above quite a bit.

--- internal/remediation/relax.go
+++ internal/remediation/relax.go
@@ -10,10 +10,68 @@ import (
 	"github.com/google/osv-scanner/internal/resolution"
 )
 
-//nolint:unused
+// ComputeRelaxPatches attempts to resolve each vulnerability found in result independently, returning the list of unique possible patches
+func ComputeRelaxPatches(ctx context.Context, cl resolve.Client, result *resolution.ResolutionResult, opts RemediationOptions) ([]resolution.ResolutionDiff, error) {
+	// Filter the original result just in case it hasn't been already
+	result.FilterVulns(opts.MatchVuln)
+
+	// Do the resolutions concurrently
+	type relaxResult struct {
+		vulnIDs []string
+		result  *resolution.ResolutionResult
+		err     error
+	}
+	ch := make(chan relaxResult)
+	doRelax := func(vulnIDs []string) {
+		res, err := tryRelaxRemediate(ctx, cl, result, vulnIDs, opts)
+		if err == nil {
+			res.FilterVulns(opts.MatchVuln)
+		}
+		ch <- relaxResult{
+			vulnIDs: vulnIDs,
+			result:  res,
+			err:     err,
+		}
+	}
+
+	toProcess := 0
+	for _, vuln := range result.Vulns {
+		// TODO: limit the number of goroutines
+		go doRelax([]string{vuln.Vulnerability.ID})
+		toProcess++
+	}
+
+	var allResults []resolution.ResolutionDiff
+	for toProcess > 0 {
+		res := <-ch
+		toProcess--
+		if errors.Is(res.err, errRelaxRemediateImpossible) { // failed because it cannot be resolved - do not add it to list
+			continue
+		}
+		if res.err != nil { // failed for some other reason - abort
+			// TODO: stop goroutines
+			return nil, res.err
+		}
+		diff := result.CalculateDiff(res.result)
+		allResults = append(allResults, diff)
+
+		// If this patch adds a new vuln, see if we can fix it also
+		// TODO: If there's more than 1 added vuln, this can possibly cause every permutation of those vulns to be computed
+		for _, added := range diff.AddedVulns {
+			go doRelax(append(slices.Clone(res.vulnIDs), added.Vulnerability.ID))
+			toProcess++
+		}
+	}
+
+	// Sort and remove duplicate patches
+	slices.SortFunc(allResults, func(a, b resolution.ResolutionDiff) int { return a.Compare(b) })
+	allResults = slices.CompactFunc(allResults, func(a, b resolution.ResolutionDiff) bool { return a.Compare(b) == 0 })
+
+	return allResults, nil
+}
+
 var errRelaxRemediateImpossible = errors.New("cannot fix vulns by relaxing")
 
-//nolint:unused
 func tryRelaxRemediate(
 	ctx context.Context,
 	cl resolve.Client,
@@ -55,7 +113,6 @@ func tryRelaxRemediate(
 	return newRes, nil
 }
 
-//nolint:unused
 func reqsToRelax(res *resolution.ResolutionResult, vulnIDs []string, opts RemediationOptions) []int {
 	toRelax := make(map[resolve.VersionKey]string)
 	for _, v := range res.Vulns {
