f517b2b4b8837605bb6411d18fe452666b58056f
Victor Granados <victor.granadosharinero@datadoghq.com>
2024-02-02 15:09:04 +0100

    ♻️Split lineposition.InJSON function

--- internal/utility/lineposition/json.go
+++ internal/utility/lineposition/json.go
@@ -9,46 +9,26 @@ import (
 	"github.com/google/osv-scanner/pkg/models"
 )
 
-func InJSON[P models.ILinePosition](groupKey string, dependencies map[string]P, lines []string, offset int) {
+var debug bool
+
+func InJSON[P models.IFilePosition](groupKey string, dependencies map[string]P, lines []string, offset int) {
+	debug = os.Getenv("debug") == "true"
 	var group, dependency string
 	var groupLevel, stack int
+
 	for lineNumber, line := range lines {
-		position := lineNumber + offset + 1
+		position := lineNumber + offset
 		if strings.Contains(line, "{") {
 			stack++
-			keyRegexp := cachedregexp.MustCompile(`"(.+)"`)
-			match := keyRegexp.FindStringSubmatch(line)
-			if len(match) == 2 {
-				key := match[1]
-				if key != "" && group != "" {
-					if stack == groupLevel+1 {
-						if dep, ok := dependencies[key]; ok {
-							dependency = key
-							if os.Getenv("debug") == "true" {
-								fmt.Fprintf(os.Stdout, "[DEPENDENCY][START] '%s' at line %d\n", dependency, position)
-							}
-							dep.SetStart(position)
-							dependencies[dependency] = dep
-						}
-					}
+			if key := retrieveKeyFromLine(line); key != "" {
+				if group != "" && stack == groupLevel+1 {
+					openDependency(key, dependencies, position, &dependency)
 				}
 				if groupKey == key {
 					if group == "" {
-						if os.Getenv("debug") == "true" {
-							fmt.Fprintf(os.Stdout, "[GROUP][START] '%s' at line %d\n", groupKey, position)
-						}
-						group = key
-						groupLevel = stack
-					} else {
-						if dep, ok := dependencies[dependency]; ok {
-							nestedDependencies := dep.GetNestedDependencies()
-							if nestedDependencies != nil {
-								if os.Getenv("debug") == "true" {
-									fmt.Fprintf(os.Stdout, "[NESTED][START] At line %d\n", position)
-								}
-								InJSON(groupKey, nestedDependencies, lines[lineNumber:], position-1)
-							}
-						}
+						openGroup(key, stack, position, &group, &groupLevel)
+					} else if dep, ok := dependencies[dependency]; ok {
+						handleNestedDependencies(lines, groupKey, dep, position)
 					}
 				}
 			}
@@ -58,23 +38,13 @@ func InJSON[P models.ILinePosition](groupKey string, dependencies map[string]P, 
 			if group != "" {
 				if stack == groupLevel {
 					if dependency != "" {
-						if dep, ok := dependencies[dependency]; ok {
-							if os.Getenv("debug") == "true" {
-								fmt.Fprintf(os.Stdout, "[DEPENDENCY][END] '%s' at line %d\n", dependency, position)
-							}
-							dep.SetEnd(position)
-							dependencies[dependency] = dep
-							dependency = ""
-						}
+						closeDependency(dependencies, position, &dependency)
 					}
 				} else if stack == groupLevel-1 {
-					if os.Getenv("debug") == "true" {
-						fmt.Fprintf(os.Stdout, "[GROUP][END] '%s' at line %d\n", groupKey, position)
-					}
-					group = ""
+					closeGroup(position, &group)
 					if offset != 0 {
-						if os.Getenv("debug") == "true" {
-							fmt.Fprintf(os.Stdout, "[NESTED][END] At line %d\n", position)
+						if debug {
+							_, _ = fmt.Fprintf(os.Stdout, "[NESTED][END] At line %d\n", position)
 						}
 
 						return
@@ -82,5 +52,62 @@ func InJSON[P models.ILinePosition](groupKey string, dependencies map[string]P, 
 				}
 			}
 		}
+	}
+}
+
+func retrieveKeyFromLine(line string) string {
+	keyRegexp := cachedregexp.MustCompile(`"(.+)"`)
+	match := keyRegexp.FindStringSubmatch(line)
+	if len(match) == 2 {
+		return match[1]
+	} else {
+		return ""
+	}
+}
+
+func openDependency[P models.IFilePosition](key string, dependencies map[string]P, position int, dependency *string) {
+	if dep, ok := dependencies[key]; ok {
+		*dependency = key
+		dep.SetStart(position + 1)
+		dependencies[*dependency] = dep
+		if debug {
+			_, _ = fmt.Fprintf(os.Stdout, "[DEPENDENCY][START] '%s' at line %d\n", *dependency, position+1)
+		}
+	}
+}
+
+func closeDependency[P models.IFilePosition](dependencies map[string]P, position int, dependency *string) {
+	if dep, ok := dependencies[*dependency]; ok {
+		dep.SetEnd(position + 1)
+		dependencies[*dependency] = dep
+		*dependency = ""
+		if debug {
+			_, _ = fmt.Fprintf(os.Stdout, "[DEPENDENCY][END] '%s' at line %d\n", *dependency, position+1)
+		}
+	}
+}
+
+func openGroup(key string, stack int, position int, group *string, groupLevel *int) {
+	*group = key
+	*groupLevel = stack
+	if debug {
+		_, _ = fmt.Fprintf(os.Stdout, "[GROUP][START] '%s' at line %d\n", *group, position+1)
+	}
+}
+
+func closeGroup(position int, group *string) {
+	*group = ""
+	if debug {
+		_, _ = fmt.Fprintf(os.Stdout, "[GROUP][END] '%s' at line %d\n", *group, position)
+	}
+}
+
+func handleNestedDependencies[P models.IFilePosition](lines []string, groupKey string, dep P, position int) {
+	nestedDependencies := dep.GetNestedDependencies()
+	if nestedDependencies != nil {
+		if debug {
+			_, _ = fmt.Fprintf(os.Stdout, "[NESTED][START] At line %d\n", position+1)
+		}
+		InJSON(groupKey, nestedDependencies, lines[position:], position)
 	}
 }
