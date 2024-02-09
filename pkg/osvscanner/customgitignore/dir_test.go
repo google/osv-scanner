3aeb844e4b4659fd745faa31ace762686aaa4ed8
Rob Ramsay <rob.ramsay@ackama.com>
2024-02-09 23:47:43 +1300

    gofmt

--- pkg/osvscanner/customgitignore/dir_test.go
+++ pkg/osvscanner/customgitignore/dir_test.go
@@ -2,17 +2,17 @@
 package customgitignore_test
 
 import (
-	"testing"
 	"os"
 	"path"
 	"path/filepath"
-	"slices"
 	"reflect"
+	"slices"
+	"testing"
 
 	"github.com/google/osv-scanner/pkg/osvscanner/customgitignore"
 
-	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-billy/v5/osfs"
+	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/format/gitignore"
 )
 
@@ -37,7 +37,6 @@ func TestGitignoreFiles(t *testing.T) {
 	if !hasMatch {
 		t.Fatalf("Expected to find a pattern matching REPO_EXCLUE_FILE from ./.git/info/exclude ")
 	}
-
 
 	// expect ./.gitignore to be processed
 	hasMatch = slices.ContainsFunc(patterns, func(p gitignore.Pattern) bool {
@@ -71,8 +70,8 @@ func TestGitignoreFiles(t *testing.T) {
 
 		// tests if pattern.patter == []string{"DIR_B_GITIGNORE"}
 		if fv.Len() == 1 && fv.Index(0).String() == "DIR_B_GITIGNORE" {
-			t.Fatalf(	"Expected not to find pattern matching DIR_B_GITIGNORE from ./dir_a/dir_b/.gitignore; " +
-								"dir_b should have been ignored by a rule in repository-root .gitignore")
+			t.Fatalf("Expected not to find pattern matching DIR_B_GITIGNORE from ./dir_a/dir_b/.gitignore; " +
+				"dir_b should have been ignored by a rule in repository-root .gitignore")
 		}
 	}
 }
@@ -82,14 +81,15 @@ func setupGitRepo(t *testing.T) string {
 	gitRepo := path.Join(t.TempDir(), "git_repo")
 
 	// create directory tree within tempdir
-  allPaths := path.Join(gitRepo, "dir_a/dir_b")
+	allPaths := path.Join(gitRepo, "dir_a/dir_b")
 	if err := os.MkdirAll(filepath.FromSlash(allPaths), 0755); err != nil {
 		t.Fatalf("could not create paths for test: %v", err)
-  }
-  allPaths = path.Join(gitRepo, ".git/info/")
+	}
+
+	allPaths = path.Join(gitRepo, ".git/info/")
 	if err := os.MkdirAll(filepath.FromSlash(allPaths), 0755); err != nil {
 		t.Fatalf("could not create paths for test: %v", err)
-  }
+	}
 
 	// initialise a git repo
 	if _, err := git.PlainInit(filepath.FromSlash(gitRepo), false); err != nil {
@@ -97,11 +97,10 @@ func setupGitRepo(t *testing.T) string {
 	}
 
 	// add .gitignore files within the tree
-	writeGitignore(t, gitRepo, ".git/info/exclude", 			"REPO_EXCLUE_FILE")
-	writeGitignore(t, gitRepo, ".gitignore", 							"ROOT_GITIGNORE\n" +
-																												"/dir_a/dir_b")
-	writeGitignore(t, gitRepo, "dir_a/.gitignore", 				"DIR_A_GITIGNORE")
-	writeGitignore(t, gitRepo, "dir_a/dir_b/.gitignore",	"DIR_B_GITIGNORE")
+	writeGitignore(t, gitRepo, ".git/info/exclude", "REPO_EXCLUE_FILE")
+	writeGitignore(t, gitRepo, ".gitignore", "ROOT_GITIGNORE\n"+"/dir_a/dir_b")
+	writeGitignore(t, gitRepo, "dir_a/.gitignore", "DIR_A_GITIGNORE")
+	writeGitignore(t, gitRepo, "dir_a/dir_b/.gitignore", "DIR_B_GITIGNORE")
 
 	return gitRepo
 }
@@ -113,14 +112,7 @@ func writeGitignore(t *testing.T, gitRepo, f, s string) {
 	}
 }
 
-
-
-	// --- NOTES BEGIN ---
-
-	// if patterns_not_include_something {
-	//		t.Fatalf("could not write file for test: %v", err)
-	// }
-
+// --- NOTES BEGIN ---
 
 // // to pick up:
 //
