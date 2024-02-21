108b2d2b981df965f1849a049cd52777e8303bb0
Rex P <106129829+another-rex@users.noreply.github.com>
2024-02-22 10:45:00 +1100

    Proof of Concept of container scanning (#808)
    PoC of container scanning using stereoscope. The feature is currently
    behind the `--experimental-oci-image` flag.

--- pkg/osvscanner/osvscanner.go
+++ pkg/osvscanner/osvscanner.go
@@ -12,6 +12,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/google/osv-scanner/internal/image"
 	"github.com/google/osv-scanner/internal/local"
 	"github.com/google/osv-scanner/internal/output"
 	"github.com/google/osv-scanner/internal/sbom"
@@ -52,6 +53,7 @@ type ExperimentalScannerActions struct {
 	ShowAllPackages       bool
 	ScanLicensesSummary   bool
 	ScanLicensesAllowlist []string
+	ScanOCIImage          string
 
 	LocalDBPath string
 }
@@ -329,6 +331,33 @@ func (m *gitIgnoreMatcher) match(absPath string, isDir bool) (bool, error) {
 	}
 
 	return m.matcher.Match(pathInGitSep, isDir), nil
+}
+
+func scanImage(r reporter.Reporter, path string) ([]scannedPackage, error) {
+	scanResults, err := image.ScanImage(r, path)
+	if err != nil {
+		return []scannedPackage{}, err
+	}
+
+	packages := make([]scannedPackage, 0)
+
+	for _, l := range scanResults.Lockfiles {
+		for _, pkgDetail := range l.Packages {
+			packages = append(packages, scannedPackage{
+				Name:      pkgDetail.Name,
+				Version:   pkgDetail.Version,
+				Commit:    pkgDetail.Commit,
+				Ecosystem: pkgDetail.Ecosystem,
+				DepGroups: pkgDetail.DepGroups,
+				Source: models.SourceInfo{
+					Path: path + ":" + l.FilePath,
+					Type: "docker",
+				},
+			})
+		}
+	}
+
+	return packages, nil
 }
 
 // scanLockfile will load, identify, and parse the lockfile path passed in, and add the dependencies specified
@@ -730,9 +759,18 @@ func DoScan(actions ScannerActions, r reporter.Reporter) (models.VulnerabilityRe
 		}
 	}
 
+	if actions.ExperimentalScannerActions.ScanOCIImage != "" {
+		r.Infof("Scanning image %s\n", actions.ExperimentalScannerActions.ScanOCIImage)
+		pkgs, err := scanImage(r, actions.ExperimentalScannerActions.ScanOCIImage)
+		if err != nil {
+			return models.VulnerabilityResults{}, err
+		}
+
+		scannedPackages = append(scannedPackages, pkgs...)
+	}
+
+	// TODO: Deprecated
 	for _, container := range actions.DockerContainerNames {
-		// TODO: Automatically figure out what docker base image
-		// and scan appropriately.
 		pkgs, _ := scanDebianDocker(r, container)
 		scannedPackages = append(scannedPackages, pkgs...)
 	}
