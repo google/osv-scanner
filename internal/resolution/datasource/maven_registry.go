bbb5737f5c586b1b6e64fcfdd31e5f1fe6e6af86
Xueqin Cui <72771658+cuixq@users.noreply.github.com>
2024-03-14 13:51:55 +1100

    improve error messages in Maven registry client (#859)
    Follow-up on https://github.com/google/osv-scanner/pull/857

--- internal/resolution/datasource/maven_registry.go
+++ internal/resolution/datasource/maven_registry.go
@@ -3,13 +3,13 @@ package datasource
 import (
 	"context"
 	"encoding/xml"
-	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
 	"strings"
 
 	"deps.dev/util/maven"
+	"github.com/google/osv-scanner/pkg/osvscanner"
 )
 
 const MavenCentral = "https://repo.maven.apache.org/maven2"
@@ -27,27 +27,27 @@ func NewMavenRegistryAPIClient() (*MavenRegistryAPIClient, error) {
 func (m *MavenRegistryAPIClient) GetProject(ctx context.Context, groupID, artifactID, version string) (maven.Project, error) {
 	url, err := url.JoinPath(m.Registry, strings.ReplaceAll(groupID, ".", "/"), artifactID, version, fmt.Sprintf("%s-%s.pom", artifactID, version))
 	if err != nil {
-		return maven.Project{}, err
+		return maven.Project{}, fmt.Errorf("failed to join path: %w", err)
 	}
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		return maven.Project{}, err
+		return maven.Project{}, fmt.Errorf("failed to make new request: %w", err)
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return maven.Project{}, err
+		return maven.Project{}, fmt.Errorf("%w: Maven registry query failed: %w", osvscanner.ErrAPIFailed, err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return maven.Project{}, errors.New(resp.Status)
+		return maven.Project{}, fmt.Errorf("%w: Maven registry query status: %s", osvscanner.ErrAPIFailed, resp.Status)
 	}
 
 	var proj maven.Project
 	if err := xml.NewDecoder(resp.Body).Decode(&proj); err != nil {
-		return maven.Project{}, err
+		return maven.Project{}, fmt.Errorf("failed to decode Maven project: %w", err)
 	}
 
 	return proj, nil
