1c0191629b829f3d09acade595abb6f51ef0609c
Michael Kedar <michaelkedar@google.com>
2024-03-26 15:22:07 +1100

    Make MockHTTPServer for tests (#888)
    Factored out the httptest.Server / handler into one struct for easier
    reuse in tests.

--- internal/resolution/datasource/npm_registry_test.go
+++ internal/resolution/datasource/npm_registry_test.go
@@ -2,63 +2,14 @@ package datasource_test
 
 import (
 	"context"
-	"log"
-	"net/http"
-	"net/http/httptest"
-	"os"
 	"path/filepath"
 	"slices"
 	"strings"
-	"sync"
 	"testing"
 
 	"github.com/google/osv-scanner/internal/resolution/datasource"
 	"github.com/google/osv-scanner/internal/testutility"
 )
-
-type fakeNpmRegistry struct {
-	mu         sync.Mutex
-	repository map[string][]byte // path -> response
-	expectAuth string
-}
-
-func (f *fakeNpmRegistry) setResponse(path string, response []byte) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	if f.repository == nil {
-		f.repository = make(map[string][]byte)
-	}
-	f.repository[path] = response
-}
-
-func (f *fakeNpmRegistry) setExpectAuth(auth string) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	f.expectAuth = auth
-}
-
-func (f *fakeNpmRegistry) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.RawPath
-	if path == "" {
-		path = r.URL.Path
-	}
-	f.mu.Lock()
-	wantAuth := f.expectAuth
-	resp, ok := f.repository[strings.TrimPrefix(path, "/")]
-	f.mu.Unlock()
-
-	if wantAuth != "" && r.Header.Get("Authorization") != wantAuth {
-		w.WriteHeader(http.StatusUnauthorized)
-		resp = []byte("unauthorized")
-	} else if !ok {
-		w.WriteHeader(http.StatusNotFound)
-		resp = []byte("not found")
-	}
-
-	if _, err := w.Write(resp); err != nil {
-		log.Fatalf("Write: %v", err)
-	}
-}
 
 func TestNpmRegistryClient(t *testing.T) {
 	t.Parallel()
@@ -69,34 +20,14 @@ func TestNpmRegistryClient(t *testing.T) {
 		authToken = "bmljZS10b2tlbgo="
 	)
 
-	reg1 := &fakeNpmRegistry{}
-	reg1.setExpectAuth("Basic " + auth)
+	srv1 := testutility.NewMockHTTPServer(t)
+	srv1.SetAuthorization(t, "Basic "+auth)
+	srv1.SetResponseFromFile(t, "/fake-package", "./fixtures/npm_registry/fake-package.json")
+	srv1.SetResponseFromFile(t, "/fake-package/2.2.2", "./fixtures/npm_registry/fake-package-2.2.2.json")
 
-	b, err := os.ReadFile("./fixtures/npm_registry/fake-package.json")
-	if err != nil {
-		t.Fatalf("failed to read fake registry response file: %v", err)
-	}
-	reg1.setResponse("fake-package", b)
-
-	b, err = os.ReadFile("./fixtures/npm_registry/fake-package-2.2.2.json")
-	if err != nil {
-		t.Fatalf("failed to read fake registry response file: %v", err)
-	}
-	reg1.setResponse("fake-package/2.2.2", b)
-
-	reg2 := &fakeNpmRegistry{}
-	reg2.setExpectAuth("Bearer " + authToken)
-
-	b, err = os.ReadFile("./fixtures/npm_registry/@fake-registry-a.json")
-	if err != nil {
-		t.Fatalf("failed to read fake registry response file: %v", err)
-	}
-	reg2.setResponse("@fake-registry%2Fa", b)
-
-	srv1 := httptest.NewServer(reg1)
-	defer srv1.Close()
-	srv2 := httptest.NewServer(reg2)
-	defer srv2.Close()
+	srv2 := testutility.NewMockHTTPServer(t)
+	srv2.SetAuthorization(t, "Bearer "+authToken)
+	srv2.SetResponseFromFile(t, "/@fake-registry%2Fa", "./fixtures/npm_registry/@fake-registry-a.json")
 
 	npmrcFile := createTempNpmrc(t, ".npmrc")
 	writeToNpmrc(t, npmrcFile,
