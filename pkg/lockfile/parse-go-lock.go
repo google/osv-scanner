559d0c9b2bc9ed6bd133fd83ca43ff265118ba19
Gareth Jones <Jones258@Gmail.com>
2024-05-01 12:18:40 +1200

    refactor: use `maps` functions instead of custom implementations (#940)
    This replaces some of our custom code for map-based operations with
    almost-standard-library versions

--- pkg/lockfile/parse-go-lock.go
+++ pkg/lockfile/parse-go-lock.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"golang.org/x/exp/maps"
 	"golang.org/x/mod/modfile"
 )
 
@@ -91,7 +92,7 @@ func (e GoLockExtractor) Extract(f DepFile) ([]PackageDetails, error) {
 		}
 	}
 
-	return pkgDetailsMapToSlice(deduplicatePackages(packages)), nil
+	return maps.Values(deduplicatePackages(packages)), nil
 }
 
 var _ Extractor = GoLockExtractor{}
