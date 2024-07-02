8f2c059a8a9fd506456f4f4592c65c926897a46c
Gareth Jones <Jones258@Gmail.com>
2024-07-02 18:18:55 +1200

    refactor: just disable color output rather than tracking terminal width (#1087)
    This lets us drop some conditionals - weirdly it doesn't seem to work if
    we move the conditional `text.DisableColors` call into `reporter.New`
    even though that does work for the vertical reporter...

--- internal/output/markdowntable.go
+++ internal/output/markdowntable.go
@@ -4,15 +4,17 @@ import (
 	"io"
 
 	"github.com/google/osv-scanner/pkg/models"
-
 	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 )
 
 // PrintTableResults prints the osv scan results into a human friendly table.
 func PrintMarkdownTableResults(vulnResult *models.VulnerabilityResults, outputWriter io.Writer) {
+	text.DisableColors()
+
 	outputTable := table.NewWriter()
 	outputTable.SetOutputMirror(outputWriter)
-	outputTable = tableBuilder(outputTable, vulnResult, false)
+	outputTable = tableBuilder(outputTable, vulnResult)
 
 	if outputTable.Length() != 0 {
 		outputTable.RenderMarkdown()
