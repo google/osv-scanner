83c1c981a65a7bc1d2e4ba594a91f7f4c7f32b18
Xueqin Cui <cuixq@google.com>
2024-08-20 10:55:37 +1000

    only tabs

--- internal/resolution/manifest/maven.go
+++ internal/resolution/manifest/maven.go
@@ -370,10 +370,9 @@ func (MavenManifestIO) Write(df lockfile.DepFile, w io.Writer, patch ManifestPat
 	return err
 }
 
-// Unescape replaces escaped tabs and newlines with unescaped characters.
+// Unescape replaces escaped tabs with unescaped characters.
 func unescape(out string) []byte {
 	out = strings.ReplaceAll(out, "&#x9;", "\t")
-	out = strings.ReplaceAll(out, "&#xA;", "\n")
 
 	return []byte(out)
 }
