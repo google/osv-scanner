0167354914b745a448bd1f49cfc003eeb03c41c5
Gareth Jones <jones258@gmail.com>
2024-09-13 12:46:29 +1200

    test: add case for coverage

--- cmd/osv-scanner/main_test.go
+++ cmd/osv-scanner/main_test.go
@@ -318,6 +318,12 @@ func TestRun(t *testing.T) {
 			args: []string{"", "--config=./fixtures/osv-scanner-composite-config.toml", "--experimental-licenses", "MIT", "./fixtures/locks-many", "./fixtures/locks-insecure", "./fixtures/maven-transitive"},
 			exit: 1,
 		},
+		// ignored vulnerabilities and packages without a reason should be called out
+		{
+			name: "ignores without reason should be explicitly called out",
+			args: []string{"", "--config=./fixtures/osv-scanner-reasonless-ignores-config.toml", "./fixtures/locks-many/package-lock.json", "./fixtures/locks-many/composer.lock"},
+			exit: 0,
+		},
 		// invalid config file
 		{
 			name: "config file is invalid",
