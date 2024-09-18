f88f82ee8505c4c94bdf75f0deea320af8866689
Gareth Jones <jones258@gmail.com>
2024-09-18 15:03:31 +1200

    refactor: move field to end of struct

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -31,10 +31,10 @@ type ConfigManager struct {
 type Config struct {
 	IgnoredVulns      []IgnoreEntry          `toml:"IgnoredVulns"`
 	PackageOverrides  []PackageOverrideEntry `toml:"PackageOverrides"`
+	GoVersionOverride string                 `toml:"GoVersionOverride"`
 	// The path to config file that this config was loaded from,
 	// set by the scanner after having successfully parsed the file
-	LoadPath          string                 `toml:"-"`
-	GoVersionOverride string                 `toml:"GoVersionOverride"`
+	LoadPath string `toml:"-"`
 }
 
 type IgnoreEntry struct {
