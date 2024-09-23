google/osv-scanner

commit 4781c4bdd4b5773b488f2cfa7066ba86e0eaf9ca

fix: add missing toml tags to struct (and update linter) (#190)
I was curious and ended up doing the work so might as well land it 🤷

This also removes one of the three "<linter> is disabled because
generics" warnings.

pkg/config/config.go
@@ -24,8 +24,8 @@ type ConfigManager struct {
 }
 
 type Config struct {
-	IgnoredVulns []IgnoreEntry
-	LoadPath     string
+	IgnoredVulns []IgnoreEntry `toml:"IgnoredVulns"`
+	LoadPath     string        `toml:"LoadPath"`
 }
 
 type IgnoreEntry struct {

.github/workflows/lint.yaml
@@ -39,5 +39,4 @@ jobs:
         uses: golangci/golangci-lint-action@v3.4.0
         with:
           # Optional: version of golangci-lint to use in form of v1.2 or v1.2.3 or `latest` to use the latest version
-          version: v1.50.1
-
+          version: v1.51.1

run_lints.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-go run github.com/golangci/golangci-lint/cmd/golangci-lint@v1.50.1 run ./... --max-same-issues 0
+go run github.com/golangci/golangci-lint/cmd/golangci-lint@v1.51.1 run ./... --max-same-issues 0

commit 67a2f048dd89d885e2afc01df2a3f037c04417a1

fix: don't allow `LoadPath` to be set via config file (#1252)
This is a pseudo bug I introduced way back in #190 - it doesn't actually
happen as our implementation always explicitly sets the property after
we've successfully parsed the file into the struct, but it is possible
for refactors like #1248 to reveal this and ultimately we don't want
this behaviour so we might as well fix it.

pkg/config/config.go
@@ -31,8 +31,10 @@ type ConfigManager struct {
 type Config struct {
 	IgnoredVulns      []IgnoreEntry          `toml:"IgnoredVulns"`
 	PackageOverrides  []PackageOverrideEntry `toml:"PackageOverrides"`
-	LoadPath          string                 `toml:"LoadPath"`
 	GoVersionOverride string                 `toml:"GoVersionOverride"`
+	// The path to config file that this config was loaded from,
+	// set by the scanner after having successfully parsed the file
+	LoadPath string `toml:"-"`
 }
 
 type IgnoreEntry struct {