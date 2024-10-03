50d4f824c022f3b247fe86c23addb25d8aa0d753
Holly Gong <gongh@google.com>
2024-10-03 14:22:36 +1000

    update fix version display and hide html from help command

--- internal/output/html.go
+++ internal/output/html.go
@@ -84,11 +84,12 @@ type HTMLVulnCount struct {
 }
 
 const UnfixedDescription = "No fix available"
+const VersionUnsupported = "N/A"
 
 // HTML templates directory
 const TemplateDir = "html/*"
 
-// supportedBaseImages lists the supported OS base images for container scanning.
+// baseImages is a list of OS images.
 var baseImages = []string{"Debian", "Alpine", "Ubuntu"}
 
 //go:embed html/*
@@ -147,7 +148,8 @@ func processSource(packageSource models.PackageSource) *HTMLSourceResult {
 
 	// Split vulnerabilities into called and uncalled.
 	// Only add one vulnerability per group
-	packageResults := processPackageResults(allVulns, groupIDs, uncalledVulnIDs, ecosystemName)
+	ecosystemPrefix := models.Ecosystem(strings.Split(ecosystemName, ":")[0])
+	packageResults := processPackageResults(allVulns, groupIDs, uncalledVulnIDs, ecosystemPrefix)
 	var count HTMLVulnCount
 	for index, packageResult := range packageResults {
 		packageResults[index].Ecosystem = ecosystemName
@@ -167,7 +169,7 @@ func processSource(packageSource models.PackageSource) *HTMLSourceResult {
 
 // processPackageResults splits the given vulnerabilities into called and uncalled
 // based on the uncalledVulnIDs map.
-func processPackageResults(allVulns []HTMLVulnResult, groupIDs map[string]models.GroupInfo, uncalledVulnIDs map[string]bool, ecosystem string) []HTMLPackageResult {
+func processPackageResults(allVulns []HTMLVulnResult, groupIDs map[string]models.GroupInfo, uncalledVulnIDs map[string]bool, ecosystemPrefix models.Ecosystem) []HTMLPackageResult {
 	packageResults := make(map[string]*HTMLPackageResult)
 	for _, vuln := range allVulns {
 		groupInfo, isIndex := groupIDs[vuln.Summary.ID]
@@ -215,7 +217,7 @@ func processPackageResults(allVulns []HTMLVulnResult, groupIDs map[string]models
 	for _, result := range packageResults {
 		if len(result.CalledVulns) > 0 {
 			result.InstalledVersion = result.CalledVulns[0].Summary.InstalledVersion
-			result.FixedVersion = getMaxFixedVersion(ecosystem, result.CalledVulns)
+			result.FixedVersion = getMaxFixedVersion(ecosystemPrefix, result.CalledVulns)
 		}
 
 		results = append(results, *result)
@@ -361,7 +363,11 @@ func uniqueIndex(index *int) func() int {
 // available, it returns "No fix available".
 func getFixVersion(allAffected []models.Affected, installedVersion string, installedPackage string, ecosystem models.Ecosystem) string {
 	ecosystemPrefix := models.Ecosystem(strings.Split(string(ecosystem), ":")[0])
-	vp := semantic.MustParse(installedVersion, ecosystemPrefix)
+	vp, err := semantic.Parse(installedVersion, ecosystemPrefix)
+	if err != nil {
+		return VersionUnsupported
+	}
+
 	minFixVersion := UnfixedDescription
 	for _, affected := range allAffected {
 		if affected.Package.Name != installedPackage || affected.Package.Ecosystem != ecosystem {
@@ -369,9 +375,12 @@ func getFixVersion(allAffected []models.Affected, installedVersion string, insta
 		}
 		for _, affectedRange := range affected.Ranges {
 			for _, affectedEvent := range affectedRange.Events {
+				// Skip if it's not a fix version event or the installed version is greater than the fix version.
 				if affectedEvent.Fixed == "" || vp.CompareStr(affectedEvent.Fixed) > 0 {
 					continue
 				}
+
+				// Find the minium fix version
 				if minFixVersion == UnfixedDescription || semantic.MustParse(affectedEvent.Fixed, ecosystemPrefix).CompareStr(minFixVersion) < 0 {
 					minFixVersion = affectedEvent.Fixed
 				}
@@ -382,14 +391,18 @@ func getFixVersion(allAffected []models.Affected, installedVersion string, insta
 	return minFixVersion
 }
 
-func getMaxFixedVersion(ecosystem string, allVulns []HTMLVulnResult) string {
-	ecosystemPrefix := models.Ecosystem(strings.Split(string(ecosystem), ":")[0]) //nolint:all
+// getMaxFixedVersion determines the highest version that resolves the most known vulnerabilities for a package.
+func getMaxFixedVersion(ecosystemPrefix models.Ecosystem, allVulns []HTMLVulnResult) string {
 	maxFixVersion := ""
 	var vp semantic.Version
 	for _, vuln := range allVulns {
-		if vuln.Summary.FixedVersion == UnfixedDescription {
-			// If one vuln is not yet fixed, the package doesn't have fix version
+		if vuln.Summary.FixedVersion == VersionUnsupported {
 			return UnfixedDescription
+		}
+
+		// Skip vulnerabilities without a fixed version.
+		if vuln.Summary.FixedVersion == UnfixedDescription {
+			continue
 		}
 
 		if maxFixVersion == "" {
@@ -399,10 +412,16 @@ func getMaxFixedVersion(ecosystem string, allVulns []HTMLVulnResult) string {
 			continue
 		}
 
+		// Update if the current vulnerability's fixed version is higher
 		if vp.CompareStr(vuln.Summary.FixedVersion) < 0 {
 			maxFixVersion = vuln.Summary.FixedVersion
 			vp = semantic.MustParse(maxFixVersion, ecosystemPrefix)
 		}
+	}
+
+	// Default to UnfixedDescription if no fix version is found.
+	if maxFixVersion == "" {
+		maxFixVersion = UnfixedDescription
 	}
 
 	return maxFixVersion
