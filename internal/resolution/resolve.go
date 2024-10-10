116acf2c16c8031b1e4d33cf2986efb1940174dc
Gareth Jones <jones258@gmail.com>
2024-10-10 13:01:03 +1300

    chore: remove `nolint` comment

--- internal/resolution/resolve.go
+++ internal/resolution/resolve.go
@@ -59,7 +59,7 @@ func (res *Result) Errors() []NodeError {
 }
 
 func getResolver(sys resolve.System, cl resolve.Client) (resolve.Resolver, error) {
-	switch sys { //nolint:exhaustive
+	switch sys {
 	case resolve.NPM:
 		return npm.NewResolver(cl), nil
 	case resolve.Maven:
