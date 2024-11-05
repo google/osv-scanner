fe73558a90f25858bbda550263816aeb33466b24
Gareth Jones <Jones258@Gmail.com>
2024-11-06 12:35:23 +1300

    test: use traditional `tt` variable name and remove unneeded loop-copy-var (#1381)
    Typically with table tests the inner variable is called `tt`, which
    we're doing in most places already but not everywhere - and as a result,
    this lead to linting picking up a few `copyloopvar` violations that were
    previously ignored due to the different variable name

--- pkg/reporter/sarif_reporter_test.go
+++ pkg/reporter/sarif_reporter_test.go
@@ -37,14 +37,14 @@ func TestSarifReporter_Warnf(t *testing.T) {
 		{lvl: reporter.ErrorLevel, expectedPrintout: ""},
 	}
 
-	for _, test := range tests {
+	for _, tt := range tests {
 		writer := &bytes.Buffer{}
-		r := reporter.NewSarifReporter(io.Discard, writer, test.lvl)
+		r := reporter.NewSarifReporter(io.Discard, writer, tt.lvl)
 
 		r.Warnf("%s", text)
 
-		if writer.String() != test.expectedPrintout {
-			t.Errorf("expected \"%s\", got \"%s\"", test.expectedPrintout, writer.String())
+		if writer.String() != tt.expectedPrintout {
+			t.Errorf("expected \"%s\", got \"%s\"", tt.expectedPrintout, writer.String())
 		}
 	}
 }
@@ -61,14 +61,14 @@ func TestSarifReporter_Infof(t *testing.T) {
 		{lvl: reporter.WarnLevel, expectedPrintout: ""},
 	}
 
-	for _, test := range tests {
+	for _, tt := range tests {
 		writer := &bytes.Buffer{}
-		r := reporter.NewSarifReporter(io.Discard, writer, test.lvl)
+		r := reporter.NewSarifReporter(io.Discard, writer, tt.lvl)
 
 		r.Infof("%s", text)
 
-		if writer.String() != test.expectedPrintout {
-			t.Errorf("expected \"%s\", got \"%s\"", test.expectedPrintout, writer.String())
+		if writer.String() != tt.expectedPrintout {
+			t.Errorf("expected \"%s\", got \"%s\"", tt.expectedPrintout, writer.String())
 		}
 	}
 }
@@ -85,14 +85,14 @@ func TestSarifReporter_Verbosef(t *testing.T) {
 		{lvl: reporter.InfoLevel, expectedPrintout: ""},
 	}
 
-	for _, test := range tests {
+	for _, tt := range tests {
 		writer := &bytes.Buffer{}
-		r := reporter.NewSarifReporter(io.Discard, writer, test.lvl)
+		r := reporter.NewSarifReporter(io.Discard, writer, tt.lvl)
 
 		r.Verbosef("%s", text)
 
-		if writer.String() != test.expectedPrintout {
-			t.Errorf("expected \"%s\", got \"%s\"", test.expectedPrintout, writer.String())
+		if writer.String() != tt.expectedPrintout {
+			t.Errorf("expected \"%s\", got \"%s\"", tt.expectedPrintout, writer.String())
 		}
 	}
 }
