e90c081633d01a214c106c4c699ae59aff687314
Rex P <rexpan@google.com>
2024-12-10 14:27:05 +1100

    Add tests for git extractor

---
+++ internal/lockfilescalibr/vcs/gitrepo/extractor_test.go
@@ -0,0 +1,70 @@
+package gitrepo_test
+
+import (
+	"context"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/google/osv-scalibr/extractor"
+	"github.com/google/osv-scalibr/testing/extracttest"
+	"github.com/google/osv-scanner/internal/lockfilescalibr/vcs/gitrepo"
+)
+
+func TestExtractor_Extract(t *testing.T) {
+	t.Parallel()
+
+	tests := []extracttest.TestTableEntry{
+		{
+			Name: "Not a git dir",
+			InputConfig: extracttest.ScanInputMockConfig{
+				Path: "testdata/example-not-git",
+			},
+			WantErr: extracttest.ContainsErrStr{Str: "repository does not exist"},
+		},
+		{
+			Name: "example git",
+			InputConfig: extracttest.ScanInputMockConfig{
+				Path: "testdata/example-git",
+			},
+			WantInventory: []*extractor.Inventory{
+				{
+					Locations: []string{"testdata/example-git"},
+					SourceCode: &extractor.SourceCodeIdentifier{
+						Commit: "862ac4bd2703b622e85f29f55a2fd8cd6caf8182",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+			extr := gitrepo.Extractor{}
+
+			err := os.Rename(path.Join(tt.InputConfig.Path, "git-hidden"), path.Join(tt.InputConfig.Path, ".git"))
+			if err != nil {
+				t.Errorf("can't find git-hidden folder")
+			}
+
+			defer os.Rename(path.Join(tt.InputConfig.Path, ".git"), path.Join(tt.InputConfig.Path, "git-hidden"))
+
+			scanInput := extracttest.GenerateScanInputMock(t, tt.InputConfig)
+			defer extracttest.CloseTestScanInput(t, scanInput)
+
+			got, err := extr.Extract(context.Background(), &scanInput)
+
+			if diff := cmp.Diff(tt.WantErr, err, cmpopts.EquateErrors()); diff != "" {
+				t.Errorf("%s.Extract(%q) error diff (-want +got):\n%s", extr.Name(), tt.InputConfig.Path, diff)
+				return
+			}
+
+			if diff := cmp.Diff(tt.WantInventory, got, cmpopts.SortSlices(extracttest.InventoryCmpLess)); diff != "" {
+				t.Errorf("%s.Extract(%q) diff (-want +got):\n%s", extr.Name(), tt.InputConfig.Path, diff)
+			}
+		})
+	}
+
+}
