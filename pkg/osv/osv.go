13bf11cde3d948266b295e8930b2246d1c9df205
Holly Gong <gongh@google.com>
2024-12-11 15:15:59 +1100

    feat: make batch queries send in parrallel

--- pkg/osv/osv.go
+++ pkg/osv/osv.go
@@ -27,9 +27,10 @@ const (
 	BaseVulnerabilityURL = "https://osv.dev/"
 	// maxQueriesPerRequest splits up querybatch into multiple requests if
 	// number of queries exceed this number
-	maxQueriesPerRequest  = 1000
-	maxConcurrentRequests = 1000
-	maxRetryAttempts      = 4
+	maxQueriesPerRequest       = 1000
+	maxConcurrentRequests      = 1000
+	maxConcurrentBatchRequests = 10
+	maxRetryAttempts           = 4
 	// jitterMultiplier is multiplied to the retry delay multiplied by rand(0, 1.0)
 	jitterMultiplier = 2
 )
@@ -181,42 +182,65 @@ func MakeRequest(request BatchedQuery) (*BatchedResponse, error) {
 func MakeRequestWithClient(request BatchedQuery, client *http.Client) (*BatchedResponse, error) {
 	// API has a limit of 1000 bulk query per request
 	queryChunks := chunkBy(request.Queries, maxQueriesPerRequest)
-	var totalOsvResp BatchedResponse
-	for _, queries := range queryChunks {
+	totalOsvRespResults := make([][]MinimalResponse, len(queryChunks))
+
+	g, ctx := errgroup.WithContext(context.TODO())
+	g.SetLimit(maxConcurrentBatchRequests)
+	for batchIndex, queries := range queryChunks {
 		requestBytes, err := json.Marshal(BatchedQuery{Queries: queries})
 		if err != nil {
 			return nil, err
 		}
 
-		resp, err := makeRetryRequest(func() (*http.Response, error) {
-			// Make sure request buffer is inside retry, if outside
-			// http request would finish the buffer, and retried requests would be empty
-			requestBuf := bytes.NewBuffer(requestBytes)
-			// We do not need a specific context
-			req, err := http.NewRequest(http.MethodPost, QueryEndpoint, requestBuf)
+		g.Go(func() error {
+			// exit early if another hydration request has already failed
+			// results are thrown away later, so avoid needless work
+			if ctx.Err() != nil {
+				return nil
+			}
+
+			resp, err := makeRetryRequest(func() (*http.Response, error) {
+				// Make sure request buffer is inside retry, if outside
+				// http request would finish the buffer, and retried requests would be empty
+				requestBuf := bytes.NewBuffer(requestBytes)
+				// We do not need a specific context
+				req, err := http.NewRequest(http.MethodPost, QueryEndpoint, requestBuf)
+				if err != nil {
+					return nil, err
+				}
+				req.Header.Set("Content-Type", "application/json")
+				if RequestUserAgent != "" {
+					req.Header.Set("User-Agent", RequestUserAgent)
+				}
+
+				return client.Do(req)
+			})
 			if err != nil {
-				return nil, err
+				return err
 			}
-			req.Header.Set("Content-Type", "application/json")
-			if RequestUserAgent != "" {
-				req.Header.Set("User-Agent", RequestUserAgent)
+			defer resp.Body.Close()
+
+			var osvResp BatchedResponse
+			decoder := json.NewDecoder(resp.Body)
+			err = decoder.Decode(&osvResp)
+			if err != nil {
+				return err
 			}
 
-			return client.Do(req)
+			// Store batch results in the corresponding index to maintain original query order.
+			totalOsvRespResults[batchIndex] = osvResp.Results
+
+			return nil
 		})
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
+	}
 
-		var osvResp BatchedResponse
-		decoder := json.NewDecoder(resp.Body)
-		err = decoder.Decode(&osvResp)
-		if err != nil {
-			return nil, err
-		}
+	if err := g.Wait(); err != nil {
+		return nil, err
+	}
 
-		totalOsvResp.Results = append(totalOsvResp.Results, osvResp.Results...)
+	var totalOsvResp BatchedResponse
+	for _, resutls := range totalOsvRespResults {
+		totalOsvResp.Results = append(totalOsvResp.Results, resutls...)
 	}
 
 	return &totalOsvResp, nil
