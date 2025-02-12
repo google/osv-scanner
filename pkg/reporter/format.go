659a8cfd455d97f2e9577076c9001de6f90db3df
Gareth Jones <jones258@gmail.com>
2025-02-13 07:44:31 +1300

    feat: remove `reporter.New`

--- pkg/reporter/format.go
+++ pkg/reporter/format.go
@@ -13,33 +13,6 @@ func Format() []string {
 	return format
 }
 
-// New returns an implementation of the reporter interface depending on the format passed in
-// set terminalWidth as 0 to indicate the output is not a terminal
-func New(format string, stdout, stderr io.Writer, level VerbosityLevel, terminalWidth int) (Reporter, error) {
-	switch format {
-	case "html":
-		return NewHTMLReporter(stdout, stderr, level), nil
-	case "json":
-		return NewJSONReporter(stdout, stderr, level), nil
-	case "vertical":
-		return NewVerticalReporter(stdout, stderr, level, false, terminalWidth), nil
-	case "table":
-		return NewTableReporter(stdout, stderr, level, false, terminalWidth), nil
-	case "markdown":
-		return NewTableReporter(stdout, stderr, level, true, terminalWidth), nil
-	case "sarif":
-		return NewSarifReporter(stdout, stderr, level), nil
-	case "gh-annotations":
-		return NewGHAnnotationsReporter(stdout, stderr, level), nil
-	case "cyclonedx-1-4":
-		return NewCycloneDXReporter(stdout, stderr, models.CycloneDXVersion14, level), nil
-	case "cyclonedx-1-5":
-		return NewCycloneDXReporter(stdout, stderr, models.CycloneDXVersion15, level), nil
-	default:
-		return nil, fmt.Errorf("%v is not a valid format", format)
-	}
-}
-
 func newResultPrinter(format string, stdout, stderr io.Writer, level VerbosityLevel, terminalWidth int) (resultPrinter, error) {
 	switch format {
 	case "html":
--- pkg/reporter/reporter_test.go
+++ pkg/reporter/reporter_test.go
@@ -4,30 +4,31 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/google/osv-scanner/v2/pkg/models"
 	"github.com/google/osv-scanner/v2/pkg/reporter"
 )
 
-func TestNew(t *testing.T) {
+func TestPrintResult(t *testing.T) {
 	t.Parallel()
 
 	for _, format := range reporter.Format() {
 		stdout := &bytes.Buffer{}
 		stderr := &bytes.Buffer{}
 
-		_, err := reporter.New(format, stdout, stderr, reporter.InfoLevel, 0)
+		err := reporter.PrintResult(&models.VulnerabilityResults{}, format, stdout, stderr, reporter.InfoLevel, 0)
 		if err != nil {
 			t.Errorf("Reporter for '%s' format not implemented", format)
 		}
 	}
 }
 
-func TestNew_UnsupportedFormatter(t *testing.T) {
+func TestPrintResult_UnsupportedFormatter(t *testing.T) {
 	t.Parallel()
 
 	stdout := &bytes.Buffer{}
 	stderr := &bytes.Buffer{}
 
-	_, err := reporter.New("unsupported", stdout, stderr, reporter.InfoLevel, 0)
+	err := reporter.PrintResult(&models.VulnerabilityResults{}, "unsupported", stdout, stderr, reporter.InfoLevel, 0)
 
 	if err == nil {
 		t.Errorf("Did not get expected error")
