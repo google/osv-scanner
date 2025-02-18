de8293cf1b6c891432521f79afd8d0a0524285f1
Michael Medin <michael@medin.name>
2025-02-18 01:04:02 +0100

    feat: Added support for severity in sarif report (#1587)
    Adds severity to sarif reports.
    
    This PR fixes https://github.com/google/osv-scanner/issues/762
    
    PS. I am not a proficient GO programmer so any and all feedback and
    ideas for improving this is appreciated.
    
    The new fields are `properties` and `severity` in the example below, I
    added severity as a string to the bundle as that is how I interpret the
    GitHub docs `A string representing a score that indicates the level of
    severity`.
    If no severity is available the field is left out.
    
    ```json
              "rules": [
                {
                  "id": "",
                  "name": "",
                  "shortDescription": {
                    "text": ""
                  },
                  "fullDescription": {
                    "text": "",
                    "markdown": ""
                  },
                  "deprecatedIds": [
                    "CVE-2022-24713",
                    "RUSTSEC-2022-0013",
                    "GHSA-m5pq-gvj9-9vr8"
                  ],
                  "help": {
                    "text": "",
                    "markdown": ""
                  },
                  "properties": {
                    "security-severity": "7.5"
                  }
                },
    
    ```
    
    This should hopefully be according to GitHub specification here:
    https://docs.github.com/en/code-security/code-scanning/integrating-with-code-scanning/sarif-support-for-code-scanning#reportingdescriptor-object
    
    ---------
    
    Co-authored-by: Rex P <106129829+another-rex@users.noreply.github.com>

--- internal/output/sarif.go
+++ internal/output/sarif.go
@@ -6,12 +6,14 @@ import (
 	"log"
 	"path/filepath"
 	"slices"
+	"strconv"
 	"strings"
 	"text/template"
 
 	"github.com/google/osv-scanner/v2/internal/identifiers"
 	"github.com/google/osv-scanner/v2/internal/url"
 	"github.com/google/osv-scanner/v2/internal/utility/results"
+	"github.com/google/osv-scanner/v2/internal/utility/severity"
 	"github.com/google/osv-scanner/v2/internal/version"
 	"github.com/google/osv-scanner/v2/pkg/models"
 	"github.com/jedib0t/go-pretty/v6/table"
@@ -276,6 +278,21 @@ func PrintSARIFReport(vulnResult *models.VulnerabilityResults, outputWriter io.W
 			WithFullDescription(sarif.NewMultiformatMessageString(longDescription).WithMarkdown(longDescription)).
 			WithMarkdownHelp(helpText).
 			WithTextHelp(helpText)
+
+		// Find the worst severity score
+		var worstScore float64 = -1
+		for _, v := range gv.AliasedVulns {
+			score, _, _ := severity.CalculateOverallScore(v.Severity)
+			if score > worstScore {
+				worstScore = score
+			}
+		}
+
+		if worstScore >= 0 {
+			var bag = sarif.NewPropertyBag()
+			bag.AddString("security-severity", strconv.FormatFloat(worstScore, 'f', -1, 64))
+			rule.WithProperties(bag.Properties)
+		}
 
 		rule.DeprecatedIds = gv.AliasedIDList
 
