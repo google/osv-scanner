0fc55f62005bd7185da9c2856d79e507e24b15cb
Rex P <rexpan@google.com>
2025-02-19 11:14:50 +1100

    Fix lints

--- internal/utility/depgroup/devgroup.go
+++ internal/utility/depgroup/devgroup.go
@@ -4,7 +4,8 @@ import "github.com/ossf/osv-schema/bindings/go/osvschema"
 
 // IsDevGroup returns if any string in groups indicates the development dependency group for the specified ecosystem.
 func IsDevGroup(sys osvschema.Ecosystem, groups []string) bool {
-	dev := ""
+	var dev string
+	//nolint: exhaustive
 	switch sys {
 	case osvschema.EcosystemPackagist, osvschema.EcosystemNPM, osvschema.EcosystemPyPI, osvschema.EcosystemPub:
 		// Also PnpmEcosystem(=NpmEcosystem) and PipenvEcosystem(=PipEcosystem).
@@ -13,8 +14,7 @@ func IsDevGroup(sys osvschema.Ecosystem, groups []string) bool {
 		dev = "build-requires"
 	case osvschema.EcosystemMaven:
 		dev = "test"
-	case osvschema.EcosystemAlpine, osvschema.EcosystemRubyGems, osvschema.EcosystemCratesIO, osvschema.EcosystemCRAN,
-		osvschema.EcosystemDebian, osvschema.EcosystemGo, osvschema.EcosystemHex, osvschema.EcosystemNuGet:
+	default:
 		// We are not able to report development dependencies for these ecosystems.
 		return false
 	}
