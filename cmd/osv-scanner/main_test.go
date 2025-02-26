3315fdc0b0d7a566d8902c3171794c97c7c9f5b8
Gareth Jones <jones258@gmail.com>
2025-02-27 11:25:54 +1300

    test: re-implement "log muffler" so that flakey log messages are not in the cli snapshots

--- cmd/osv-scanner/main_test.go
+++ cmd/osv-scanner/main_test.go
@@ -26,11 +26,41 @@ type cliTestCase struct {
 	replaceRules []testutility.JSONReplaceRule
 }
 
+// muffledHandler eats certain log messages to reduce noise in the test output
+type muffledWriter struct {
+	*bytes.Buffer
+}
+
+func (m muffledWriter) Write(p []byte) (int, error) {
+	// todo: work with the osv-scalibr team to see if we can reduce these
+	for _, prefix := range []string{
+		"Starting filesystem walk for root:",
+		"End status: ",
+		"Neither CPE nor PURL found for package",
+		"Invalid PURL",
+		"os-release[ID] not set, fallback to",
+		"VERSION_ID not set in os-release",
+		"osrelease.ParseOsRelease(): file does not exist",
+	} {
+		if bytes.HasPrefix(p, []byte(prefix)) {
+			_, err := m.Buffer.Write([]byte("<removed>"))
+
+			return len(p), err
+		}
+	}
+
+	return m.Buffer.Write(p)
+}
+
+func newMuffledWriter() muffledWriter {
+	return muffledWriter{Buffer: &bytes.Buffer{}}
+}
+
 func runCli(t *testing.T, tc cliTestCase) (string, string) {
 	t.Helper()
 
-	stdout := &bytes.Buffer{}
-	stderr := &bytes.Buffer{}
+	stdout := newMuffledWriter()
+	stderr := newMuffledWriter()
 
 	ec := run(tc.args, stdout, stderr)
 
--- cmd/osv-scanner/testmain_test.go
+++ cmd/osv-scanner/testmain_test.go
@@ -1,51 +1,14 @@
 package main
 
 import (
-	"context"
-	"io"
-	"log"
-	"log/slog"
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/google/osv-scanner/v2/internal/testutility"
 )
 
-// muffledHandler eats certain log messages to reduce noise in the test output
-type muffledHandler struct {
-	slog.TextHandler
-}
-
-func (c *muffledHandler) Handle(ctx context.Context, record slog.Record) error {
-	if record.Level < slog.LevelError {
-		// todo: work with the osv-scalibr team to see if we can reduce these
-		for _, prefix := range []string{
-			"Starting filesystem walk for root:",
-			"End status: ",
-			"Neither CPE nor PURL found for package",
-			"Invalid PURL",
-			"os-release[ID] not set, fallback to",
-			"VERSION_ID not set in os-release",
-			"osrelease.ParseOsRelease(): file does not exist",
-		} {
-			if strings.HasPrefix(record.Message, prefix) {
-				return nil
-			}
-		}
-	}
-
-	return c.TextHandler.Handle(ctx, record)
-}
-
-func newMuffledHandler(w io.Writer) *muffledHandler {
-	return &muffledHandler{TextHandler: *slog.NewTextHandler(w, nil)}
-}
-
 func TestMain(m *testing.M) {
-	slog.SetDefault(slog.New(newMuffledHandler(log.Writer())))
-
 	// ensure a git repository doesn't already exist in the fixtures directory,
 	// in case we didn't get a chance to clean-up properly in the last run
 	os.RemoveAll("./fixtures/.git")
