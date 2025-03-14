c94be596cc63058acc7fca93dc8b56ebd3b578aa
Holly Gong <gongh@google.com>
2025-03-14 15:34:25 +1100

    remove all-pacakges flag and update tests

--- cmd/osv-scanner/main_test.go
+++ cmd/osv-scanner/main_test.go
@@ -838,8 +838,8 @@ func Test_run_OCIImageAllPackagesJSON(t *testing.T) {
 			},
 		},
 		{
-			name: "scanning image with go binary",
-			args: []string{"", "scan", "image", "--archive", "--all-packages", "--format=json", "../../internal/image/fixtures/test-ubuntu.tar"},
+			name: "scanning ubuntu image in json format",
+			args: []string{"", "scan", "image", "--archive", "--format=json", "../../internal/image/fixtures/test-ubuntu.tar"},
 			exit: 1,
 			replaceRules: []testutility.JSONReplaceRule{
 				testutility.GroupsAsArrayLen,
