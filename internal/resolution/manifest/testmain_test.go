257905c99a2f27e62be472eeebb0c5fe0dab60df
Gareth Jones <jones258@gmail.com>
2025-04-01 09:14:45 +1300

    test: remove unneeded calls to `os.Exit`

--- internal/resolution/manifest/testmain_test.go
+++ internal/resolution/manifest/testmain_test.go
@@ -1,16 +1,13 @@
 package manifest_test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/google/osv-scanner/v2/internal/testutility"
 )
 
 func TestMain(m *testing.M) {
-	code := m.Run()
+	m.Run()
 
 	testutility.CleanSnapshots(m)
-
-	os.Exit(code)
 }
