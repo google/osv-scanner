8062ef6825eabe6c007abc66bea3ec522d38466a
Gareth Jones <jones258@gmail.com>
2025-04-02 10:14:04 +1300

    feat: attempt to provide in actual results

--- internal/output/spdx.go
+++ internal/output/spdx.go
@@ -6,12 +6,40 @@ import (
 
 	scalibr "github.com/google/osv-scalibr"
 	"github.com/google/osv-scalibr/converter"
+	"github.com/google/osv-scalibr/detector"
+	"github.com/google/osv-scalibr/extractor"
+	"github.com/google/osv-scanner/v2/internal/scalibrextract/ecosystemmock"
 	"github.com/google/osv-scanner/v2/pkg/models"
 )
 
 // PrintSPDXResults writes results to the provided writer in SPDX format
 func PrintSPDXResults(vulnResult *models.VulnerabilityResults, outputWriter io.Writer) error {
-	doc := converter.ToSPDX23(&scalibr.ScanResult{}, converter.SPDXConfig{})
+	scanResult := &scalibr.ScanResult{}
+
+	for _, source := range vulnResult.Results {
+		for _, pkg := range source.Packages {
+			scanResult.Inventories = append(scanResult.Inventories, &extractor.Inventory{
+				Name:    pkg.Package.Name,
+				Version: pkg.Package.Version,
+				Extractor: ecosystemmock.Extractor{
+					MockEcosystem: pkg.Package.Ecosystem,
+				},
+			})
+			for _, vuln := range pkg.Vulnerabilities {
+				scanResult.Findings = append(scanResult.Findings, &detector.Finding{
+					Adv: &detector.Advisory{
+						ID: &detector.AdvisoryID{
+							Reference: vuln.ID,
+						},
+						Title:       vuln.Summary,
+						Description: vuln.Details,
+					},
+				})
+			}
+		}
+	}
+
+	doc := converter.ToSPDX23(scanResult, converter.SPDXConfig{})
 
 	encoder := json.NewEncoder(outputWriter)
 	encoder.SetIndent("", "  ")
