83d0a3e8e62a82652401c9578640dd3c31f8f2e2
Gareth Jones <jones258@gmail.com>
2025-04-22 10:51:58 +1200

    refactor: introduce building specific extractors by name

--- pkg/osvscanner/internal/scanners/lockfile_test.go
+++ pkg/osvscanner/internal/scanners/lockfile_test.go
@@ -11,7 +11,7 @@ func TestLockfileScalibrMappingExists(t *testing.T) {
 	// Every lockfileExtractor should have a mapping,
 	// this might not be true the other way around as some extractors are dynamically set,
 	// and not present in lockfileExtractors
-	for _, target := range lockfileExtractors {
+	for _, target := range buildLockfileExtractors(nil, nil) {
 		found := false
 		for _, names := range lockfileExtractorMapping {
 			if slices.Contains(names, target.Name()) {
