5e5ff2447f249160d4e8af7ee5eaad253c72962a
Gareth Jones <jones258@gmail.com>
2025-04-23 09:19:41 +1200

    test: move `source` cases into subpackage

---
+++ cmd/osv-scanner/scan/source/testmain_test.go
@@ -0,0 +1,16 @@
+package source_test
+
+import (
+	"log/slog"
+	"testing"
+
+	"github.com/google/osv-scanner/v2/internal/testlogger"
+	"github.com/google/osv-scanner/v2/internal/testutility"
+)
+
+func TestMain(m *testing.M) {
+	slog.SetDefault(slog.New(testlogger.New()))
+	m.Run()
+
+	testutility.CleanSnapshots(m)
+}
