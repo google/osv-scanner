f2945555e9b6873697e1dc77b2bcb1d529ae61b0
Gareth Jones <jones258@gmail.com>
2025-06-03 13:24:28 +1200

    test: setup `.git` directory

--- cmd/osv-scanner/scan/testmain_test.go
+++ cmd/osv-scanner/scan/testmain_test.go
@@ -2,8 +2,10 @@ package scan_test
 
 import (
 	"log/slog"
+	"os"
 	"testing"
 
+	"github.com/go-git/go-git/v5"
 	"github.com/google/osv-scanner/v2/cmd/osv-scanner/internal/cmd"
 	"github.com/google/osv-scanner/v2/cmd/osv-scanner/internal/testcmd"
 	"github.com/google/osv-scanner/v2/cmd/osv-scanner/scan"
@@ -12,9 +14,21 @@ import (
 )
 
 func TestMain(m *testing.M) {
+	// ensure a git repository doesn't already exist in the fixtures directory,
+	// in case we didn't get a chance to clean-up properly in the last run
+	os.RemoveAll("./fixtures/.git")
+
+	// Temporarily make the fixtures folder a git repository to prevent gitignore files messing with tests.
+	_, err := git.PlainInit("./fixtures", false)
+	if err != nil {
+		panic(err)
+	}
+
 	slog.SetDefault(slog.New(testlogger.New()))
 	testcmd.CommandsUnderTest = []cmd.CommandBuilder{scan.Command}
 	m.Run()
 
 	testutility.CleanSnapshots(m)
+
+	os.RemoveAll("./fixtures/.git")
 }
