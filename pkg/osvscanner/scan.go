9a2afcd1cb49064291153d3f5f5919f3eb01cd9f
Rex P <rexpan@google.com>
2025-06-05 15:12:53 +1000

    Revert scan.go changes

--- pkg/osvscanner/scan.go
+++ pkg/osvscanner/scan.go
@@ -1,21 +1,15 @@
 package osvscanner
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"log/slog"
 	"path/filepath"
-	"runtime"
 	"slices"
-	"strings"
 
-	scalibr "github.com/google/osv-scalibr"
 	"github.com/google/osv-scalibr/extractor"
 	"github.com/google/osv-scalibr/extractor/filesystem"
 	"github.com/google/osv-scalibr/extractor/filesystem/language/java/pomxmlnet"
-	"github.com/google/osv-scalibr/fs"
-	"github.com/google/osv-scalibr/plugin"
 	"github.com/google/osv-scanner/v2/internal/builders"
 	"github.com/google/osv-scanner/v2/internal/imodels"
 	"github.com/google/osv-scanner/v2/internal/scalibrextract"
@@ -34,14 +28,6 @@ func configureExtractors(extractors []filesystem.Extractor, accessors ExternalAc
 				MavenRegistryAPIClient: accessors.MavenRegistryAPIClient,
 			})
 		}
-
-		//if accessors.DependencyClients[osvschema.EcosystemPyPI] != nil && accessors.MavenRegistryAPIClient != nil {
-		//	requirementsenhancable.EnhanceIfPossible(tor, requirementsnet.Config{
-		//			Extractor: tor,
-		//			Client: ,
-		//		},
-		//	))
-		//}
 
 		// todo: the "disabled" aspect should probably be worked into the extractor being present in the first place
 		//  since "IncludeRootGit" is always true
@@ -122,69 +108,13 @@ func scan(accessors ExternalAccessors, actions ScannerActions) ([]imodels.Packag
 		actions,
 	)
 
-	scanner := scalibr.New()
-
-	// Build list of paths for each root
-	// On linux this would return a map with just one entry of /
-	rootMap := map[string][]string{}
-	for _, path := range actions.DirectoryPaths {
-		slog.Info(fmt.Sprintf("Scanning dir %s", path))
-
-		absPath, err := filepath.Abs(path)
+	for _, dir := range actions.DirectoryPaths {
+		slog.Info("Scanning dir " + dir)
+		pkgs, err := scanners.ScanDir(dir, actions.Recursive, !actions.NoIgnore, dirExtractors)
 		if err != nil {
 			return nil, err
 		}
-		root := getRootDir(absPath)
-		rootMap[root] = append(rootMap[root], absPath)
-	}
-
-	for root, paths := range rootMap {
-		capabilities := plugin.Capabilities{
-			DirectFS:      true,
-			RunningSystem: true,
-		}
-
-		if actions.CompareOffline {
-			capabilities.Network = plugin.NetworkOffline
-		} else {
-			capabilities.Network = plugin.NetworkOnline
-		}
-
-		if runtime.GOOS == "windows" {
-			capabilities.OS = plugin.OSWindows
-		} else {
-			capabilities.OS = plugin.OSUnix
-		}
-
-		sr := scanner.Scan(context.Background(), &scalibr.ScanConfig{
-			FilesystemExtractors:  dirExtractors,
-			StandaloneExtractors:  nil,
-			Detectors:             nil,
-			Capabilities:          &capabilities,
-			ScanRoots:             fs.RealFSScanRoots(root),
-			PathsToExtract:        paths,
-			IgnoreSubDirs:         !actions.Recursive,
-			DirsToSkip:            nil,
-			SkipDirRegex:          nil,
-			SkipDirGlob:           nil,
-			UseGitignore:          !actions.NoIgnore,
-			Stats:                 FileOpenedPrinter{},
-			ReadSymlinks:          false,
-			MaxInodes:             0,
-			StoreAbsolutePath:     true,
-			PrintDurationAnalysis: false,
-			ErrorOnFSErrors:       false,
-		})
-		if sr.Status.Status != plugin.ScanStatusSucceeded {
-			return nil, errors.New(sr.Status.FailureReason)
-		}
-
-		for _, pkg := range sr.Inventory.Packages {
-			for i := range pkg.Locations {
-				pkg.Locations[i] = filepath.Join(root, pkg.Locations[i])
-			}
-			scannedInventories = append(scannedInventories, pkg)
-		}
+		scannedInventories = append(scannedInventories, pkgs...)
 	}
 
 	// Add on additional direct dependencies passed straight from ScannerActions:
@@ -211,19 +141,4 @@ func scan(accessors ExternalAccessors, actions ScannerActions) ([]imodels.Packag
 	}
 
 	return packages, nil
-}
-
-// getRootDir returns the root directory on each system.
-// On Unix systems, it'll be /
-// On Windows, it will most likely be the drive (e.g. C:\)
-func getRootDir(path string) string {
-	if runtime.GOOS == "windows" {
-		return filepath.VolumeName(path) + "\\"
-	}
-
-	if strings.HasPrefix(path, "/") {
-		return "/"
-	}
-
-	return ""
 }
