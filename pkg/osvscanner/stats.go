7f8555dfc5d05bde7e39739ee94e437d1d3a4ffd
Rex P <106129829+another-rex@users.noreply.github.com>
2025-06-18 11:19:43 +1000

    feat: Use OSV-Scalibr scan function directly (#1936)
    Closes #1769 
    
    Behaviour changes / notes:
    - OSV-Scalibr's Scan() function does DFS rather than BFS, so the order
    of files being read in the snapshot changes slightly.
    - Their Scan() function also does not deduplicate the output of the
    extractor, so the SBOM with duplicates will return double the results. I
    think it's fine to have that behavior change for now unless someone
    complains about it.
    - Unfortunately, we also double log the path to the file in the error,
    because both the extractor and the walker both prepend the path the
    error originated from. This is a TODO in osv-scalibr to remove input
    path references in the extractor errors.
    (https://github.com/google/osv-scalibr/issues/821)
    
    ---------
    
    Co-authored-by: Mend Renovate <bot@renovateapp.com>

--- pkg/osvscanner/stats.go
+++ pkg/osvscanner/stats.go
@@ -3,6 +3,7 @@ package osvscanner
 import (
 	"fmt"
 	"log/slog"
+	"path/filepath"
 
 	"github.com/google/osv-scalibr/stats"
 	"github.com/google/osv-scanner/v2/internal/output"
@@ -15,11 +16,15 @@ type FileOpenedPrinter struct {
 var _ stats.Collector = &FileOpenedPrinter{}
 
 func (c FileOpenedPrinter) AfterExtractorRun(_ string, extractorstats *stats.AfterExtractorStats) {
+	if extractorstats.Error != nil { // Don't log scanned if error occurred
+		return
+	}
+
 	pkgsFound := len(extractorstats.Inventory.Packages)
 
 	slog.Info(fmt.Sprintf(
 		"Scanned %s file and found %d %s",
-		extractorstats.Path,
+		filepath.Join(extractorstats.Root, extractorstats.Path),
 		pkgsFound,
 		output.Form(pkgsFound, "package", "packages"),
 	))
