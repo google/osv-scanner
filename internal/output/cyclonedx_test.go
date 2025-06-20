a83a8eef0c6643ef09cd9807040ce99801d64986
Alexandre Laroche <larochealexandre@hotmail.com>
2025-06-20 08:20:42 -0400

    Add cyclonedx 1.6 in report format

--- internal/output/cyclonedx_test.go
+++ internal/output/cyclonedx_test.go
@@ -9,104 +9,40 @@ import (
 	"github.com/google/osv-scanner/v2/pkg/models"
 )
 
-func TestPrintCycloneDX14Results_WithVulnerabilities(t *testing.T) {
-	t.Parallel()
-
-	testOutputWithVulnerabilities(t, func(t *testing.T, args outputTestCaseArgs) {
+func testCycloneDXResults(t *testing.T, version models.CycloneDXVersion, testFunc func(*testing.T, func(*testing.T, outputTestCaseArgs))) {
+	t.Helper()
+	testFunc(t, func(t *testing.T, args outputTestCaseArgs) {
 		t.Helper()
-
 		outputWriter := &bytes.Buffer{}
-		err := output.PrintCycloneDXResults(args.vulnResult, models.CycloneDXVersion14, outputWriter)
-
+		err := output.PrintCycloneDXResults(args.vulnResult, version, outputWriter)
 		if err != nil {
 			t.Errorf("%v", err)
 		}
-
 		testutility.NewSnapshot().MatchText(t, outputWriter.String())
 	})
 }
 
-func TestPrintCycloneDX14Results_WithLicenseViolations(t *testing.T) {
-	t.Parallel()
+func TestPrintCycloneDXResults(t *testing.T) {
+	tests := []struct {
+		name    string
+		version models.CycloneDXVersion
+		testFn  func(*testing.T, func(*testing.T, outputTestCaseArgs))
+	}{
+		{"CycloneDX14_WithVulnerabilities", models.CycloneDXVersion14, testOutputWithVulnerabilities},
+		{"CycloneDX14_WithLicenseViolations", models.CycloneDXVersion14, testOutputWithLicenseViolations},
+		{"CycloneDX14_WithMixedIssues", models.CycloneDXVersion14, testOutputWithMixedIssues},
+		{"CycloneDX15_WithVulnerabilities", models.CycloneDXVersion15, testOutputWithVulnerabilities},
+		{"CycloneDX15_WithLicenseViolations", models.CycloneDXVersion15, testOutputWithLicenseViolations},
+		{"CycloneDX15_WithMixedIssues", models.CycloneDXVersion15, testOutputWithMixedIssues},
+		{"CycloneDX16_WithVulnerabilities", models.CycloneDXVersion16, testOutputWithVulnerabilities},
+		{"CycloneDX16_WithLicenseViolations", models.CycloneDXVersion16, testOutputWithLicenseViolations},
+		{"CycloneDX16_WithMixedIssues", models.CycloneDXVersion16, testOutputWithMixedIssues},
+	}
 
-	testOutputWithLicenseViolations(t, func(t *testing.T, args outputTestCaseArgs) {
-		t.Helper()
-
-		outputWriter := &bytes.Buffer{}
-		err := output.PrintCycloneDXResults(args.vulnResult, models.CycloneDXVersion14, outputWriter)
-
-		if err != nil {
-			t.Errorf("%v", err)
-		}
-
-		testutility.NewSnapshot().MatchText(t, outputWriter.String())
-	})
-}
-
-func TestPrintCycloneDX14Results_WithMixedIssues(t *testing.T) {
-	t.Parallel()
-
-	testOutputWithMixedIssues(t, func(t *testing.T, args outputTestCaseArgs) {
-		t.Helper()
-
-		outputWriter := &bytes.Buffer{}
-		err := output.PrintCycloneDXResults(args.vulnResult, models.CycloneDXVersion14, outputWriter)
-
-		if err != nil {
-			t.Errorf("%v", err)
-		}
-
-		testutility.NewSnapshot().MatchText(t, outputWriter.String())
-	})
-}
-
-func TestPrintCycloneDX15Results_WithVulnerabilities(t *testing.T) {
-	t.Parallel()
-
-	testOutputWithVulnerabilities(t, func(t *testing.T, args outputTestCaseArgs) {
-		t.Helper()
-
-		outputWriter := &bytes.Buffer{}
-		err := output.PrintCycloneDXResults(args.vulnResult, models.CycloneDXVersion15, outputWriter)
-
-		if err != nil {
-			t.Errorf("%v", err)
-		}
-
-		testutility.NewSnapshot().MatchText(t, outputWriter.String())
-	})
-}
-
-func TestPrintCycloneDX15Results_WithLicenseViolations(t *testing.T) {
-	t.Parallel()
-
-	testOutputWithLicenseViolations(t, func(t *testing.T, args outputTestCaseArgs) {
-		t.Helper()
-
-		outputWriter := &bytes.Buffer{}
-		err := output.PrintCycloneDXResults(args.vulnResult, models.CycloneDXVersion15, outputWriter)
-
-		if err != nil {
-			t.Errorf("%v", err)
-		}
-
-		testutility.NewSnapshot().MatchText(t, outputWriter.String())
-	})
-}
-
-func TestPrintCycloneDX15Results_WithMixedIssues(t *testing.T) {
-	t.Parallel()
-
-	testOutputWithMixedIssues(t, func(t *testing.T, args outputTestCaseArgs) {
-		t.Helper()
-
-		outputWriter := &bytes.Buffer{}
-		err := output.PrintCycloneDXResults(args.vulnResult, models.CycloneDXVersion15, outputWriter)
-
-		if err != nil {
-			t.Errorf("%v", err)
-		}
-
-		testutility.NewSnapshot().MatchText(t, outputWriter.String())
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			testCycloneDXResults(t, tt.version, tt.testFn)
+		})
+	}
 }
