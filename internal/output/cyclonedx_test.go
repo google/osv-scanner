4fbff018ec0c04aaafc4f1b68b54ef13752a8a74
Rex P <106129829+another-rex@users.noreply.github.com>
2025-06-25 11:21:11 +1000

    Update internal/output/cyclonedx_test.go

--- internal/output/cyclonedx_test.go
+++ internal/output/cyclonedx_test.go
@@ -23,6 +23,7 @@ func testCycloneDXResults(t *testing.T, version models.CycloneDXVersion, testFun
 }
 
 func TestPrintCycloneDXResults(t *testing.T) {
+  t.Parallel()
 	tests := []struct {
 		name    string
 		version models.CycloneDXVersion
