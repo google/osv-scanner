d5bdc95cef8eb790e545f5f7336283c129fdea6d
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-07-10 15:10:23 +1200

    refactor: inline `build` function

--- internal/builders/extractors.go
+++ internal/builders/extractors.go
@@ -11,40 +11,36 @@ import (
 	"github.com/google/osv-scanner/v2/internal/scalibrextract/vcs/gitrepo"
 )
 
-func build(name string) []filesystem.Extractor {
-	switch name {
-	// Java
-	case pomxmlenhanceable.Name:
-		return []filesystem.Extractor{pomxmlenhanceable.New()}
-	// Javascript
-	case nodemodules.Name:
-		return []filesystem.Extractor{nodemodules.Extractor{}}
-	// Python
-	case requirementsenhancable.Name:
-		return []filesystem.Extractor{requirementsenhancable.New()}
-	// Directories
-	case vendored.Name:
-		return []filesystem.Extractor{&vendored.Extractor{}}
-	case gitrepo.Name:
-		return []filesystem.Extractor{&gitrepo.Extractor{}}
-	}
-
-	extractors, err := list.ExtractorsFromName(name)
-
-	if err != nil {
-		cmdlogger.Errorf("%s", err)
-
-		return nil
-	}
-
-	return extractors
-}
-
 func BuildExtractors(names []string) []filesystem.Extractor {
 	extractors := make([]filesystem.Extractor, 0, len(names))
 
 	for _, name := range names {
-		extractors = append(extractors, build(name)...)
+		switch name {
+		// Java
+		case pomxmlenhanceable.Name:
+			extractors = append(extractors, pomxmlenhanceable.New())
+		// Javascript
+		case nodemodules.Name:
+			extractors = append(extractors, nodemodules.Extractor{})
+		// Python
+		case requirementsenhancable.Name:
+			extractors = append(extractors, requirementsenhancable.New())
+		// Directories
+		case vendored.Name:
+			extractors = append(extractors, &vendored.Extractor{})
+		case gitrepo.Name:
+			extractors = append(extractors, &gitrepo.Extractor{})
+		default:
+			extras, err := list.ExtractorsFromName(name)
+
+			if err != nil {
+				cmdlogger.Errorf("%s", err)
+
+				continue
+			}
+
+			extractors = append(extractors, extras...)
+		}
 	}
 
 	return extractors
