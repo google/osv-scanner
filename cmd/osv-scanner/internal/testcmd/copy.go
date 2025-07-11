cb7f83c873a4a1adde936c1683fa2b6610294f83
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-07-11 16:19:58 +1200

    chore: update comment

--- cmd/osv-scanner/internal/testcmd/copy.go
+++ cmd/osv-scanner/internal/testcmd/copy.go
@@ -24,7 +24,11 @@ func CopyFileTo(t *testing.T, file, dir string) string {
 
 // CopyFileFlagTo creates a copy of the file pointed to by the given flag (if present
 // in the test case arguments) in the given directory, updating all references
-// in the arguments before returning the new path
+// in the arguments before returning the new path.
+//
+// Values that include "does_not_exist" are assumed to be for testing when the
+// flag is given the path to a file or directory that does not exist, and so
+// are ignored as if the flag was not given a value at all
 func CopyFileFlagTo(t *testing.T, tc Case, flagName string, dir string) string {
 	t.Helper()
 
