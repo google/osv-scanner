c7499c28e1fe6546f913c1d5aa1e1f747224b7e9
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-07 12:51:19 +1200

    feat: use extractor presets provided by scalibr (#2103)
    Besides deduplicating some code between our two libraries, this'll mean
    new extractors added to scalibr will get automatically enabled as we
    update rather than requiring us to manually add them.
    
    I have made some minor modifications to the presets in the interest of a
    smoother landing, namely
    - removing the `secrets` extractor from all presets since it reads a
    bunch of generic files like TOML and JSON files but only returns invs
    when there's a finding so we'd end up with stuff like `Extracted 0
    packages from package-lock.json\nExtracted 4 packages from
    package-lock.json` (the second line being the `packagelockjson`
    extractor)
    - ~ultimately, I actually think this should be a detector rather than an
    extractor~ I'm wrong on this, but I still think we should tackle adding
    it as part of a wider "support extractors that extract secrets" PR
    - removing the `packagejson` extractor from the `artifact` preset as not
    only does it find files in caches created by the JS package managers,
    but it'll typically find the managers themselves especially as `npm`
    ships with Node by default so its always present
    - long-run it's probably something we should just have enabled, as the
    findings are technically valid and with containers you'd be chucking
    caches out and copying built files from multi-stage builds anyway, but I
    feel that should be its own PR
    - removing the `cargotoml` extractor from the `lockfile` preset as it
    doesn't really make sense to have in parallel with the `cargolock` and
    `cargoauditable` extractors
      - ~adding the `cargoauditable` extractor to the `artifact` preset~
    - ~I think this might just be an oversight in scalibr, so can do a PR
    for that soon (https://github.com/google/osv-scalibr/pull/930)~
    
    I have also for now kept our "lockfile" preset even though its
    equivalent in scalibr is "sourcecode" as with this all being
    experimental stuff it didn't seem super important to change as part of
    this PR

--- internal/scalibrplugin/extractors_parser.go
+++ internal/scalibrplugin/extractors_parser.go
@@ -2,39 +2,17 @@
 package scalibrplugin
 
 import (
+	"maps"
+
 	"github.com/google/osv-scalibr/extractor/filesystem"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/cpp/conanlock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/dart/pubspec"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/dotnet/depsjson"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/dotnet/packagesconfig"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/dotnet/packageslockjson"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/erlang/mixlock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/golang/gobinary"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/golang/gomod"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/haskell/cabal"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/haskell/stacklock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/java/archive"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/java/gradlelockfile"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/java/gradleverificationmetadataxml"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/bunlock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/packagelockjson"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/pnpmlock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/yarnlock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/php/composerlock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/python/pdmlock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/python/pipfilelock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/python/poetrylock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/python/uvlock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/python/wheelegg"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/r/renvlock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/ruby/gemfilelock"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/rust/cargoauditable"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/rust/cargolock"
+	"github.com/google/osv-scalibr/extractor/filesystem/language/java/pomxml"
+	"github.com/google/osv-scalibr/extractor/filesystem/language/java/pomxmlnet"
+	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/packagejson"
+	"github.com/google/osv-scalibr/extractor/filesystem/language/python/requirements"
+	"github.com/google/osv-scalibr/extractor/filesystem/language/python/requirementsnet"
+	"github.com/google/osv-scalibr/extractor/filesystem/language/rust/cargotoml"
 	"github.com/google/osv-scalibr/extractor/filesystem/list"
-	"github.com/google/osv-scalibr/extractor/filesystem/os/apk"
-	"github.com/google/osv-scalibr/extractor/filesystem/os/dpkg"
-	"github.com/google/osv-scalibr/extractor/filesystem/sbom/cdx"
-	"github.com/google/osv-scalibr/extractor/filesystem/sbom/spdx"
+	"github.com/google/osv-scalibr/extractor/filesystem/secrets"
 	"github.com/google/osv-scanner/v2/internal/builders"
 	"github.com/google/osv-scanner/v2/internal/scalibrextract/filesystem/vendored"
 	"github.com/google/osv-scanner/v2/internal/scalibrextract/language/java/pomxmlenhanceable"
@@ -44,85 +22,49 @@ import (
 )
 
 var ExtractorPresets = map[string]list.InitMap{
-	"sbom": {
-		spdx.Name: {spdx.New},
-		cdx.Name:  {cdx.New},
-	},
-	"lockfile": {
-		// C
-		conanlock.Name: {conanlock.New},
-
-		// Erlang
-		mixlock.Name: {mixlock.New},
-
-		// Flutter
-		pubspec.Name: {pubspec.New},
-
-		// Go
-		gomod.Name: {gomod.New},
-
-		// Java
-		gradlelockfile.Name:                {gradlelockfile.New},
-		gradleverificationmetadataxml.Name: {gradleverificationmetadataxml.New},
-		pomxmlenhanceable.Name:             {pomxmlenhanceable.New},
-
-		// Javascript
-		packagelockjson.Name: {packagelockjson.NewDefault},
-		pnpmlock.Name:        {pnpmlock.New},
-		yarnlock.Name:        {yarnlock.New},
-		bunlock.Name:         {bunlock.New},
-
-		// PHP
-		composerlock.Name: {composerlock.New},
-
-		// Python
-		pipfilelock.Name:            {pipfilelock.New},
-		pdmlock.Name:                {pdmlock.New},
-		poetrylock.Name:             {poetrylock.New},
-		requirementsenhancable.Name: {requirementsenhancable.New},
-		uvlock.Name:                 {uvlock.New},
-
-		// R
-		renvlock.Name: {renvlock.New},
-
-		// Ruby
-		gemfilelock.Name: {gemfilelock.New},
-
-		// Rust
-		cargolock.Name: {cargolock.New},
-
-		// NuGet
-		depsjson.Name:         {depsjson.NewDefault},
-		packagesconfig.Name:   {packagesconfig.NewDefault},
-		packageslockjson.Name: {packageslockjson.NewDefault},
-
-		// Haskell
-		cabal.Name:     {cabal.NewDefault},
-		stacklock.Name: {stacklock.NewDefault},
-	},
+	"sbom": list.SBOM,
+	"lockfile": concat(
+		without(list.SourceCode, []string{
+			pomxml.Name, pomxmlnet.Name,
+			requirements.Name, requirementsnet.Name,
+			secrets.Name, cargotoml.Name,
+		}),
+		list.InitMap{
+			pomxmlenhanceable.Name:      {pomxmlenhanceable.New},
+			requirementsenhancable.Name: {requirementsenhancable.New},
+		},
+	),
 	"directory": {
 		gitrepo.Name:  {gitrepo.New},
 		vendored.Name: {vendored.New},
 	},
-	"artifact": {
-		// --- Project artifacts ---
-		// Python
-		wheelegg.Name: {wheelegg.NewDefault},
-		// Java
-		archive.Name: {archive.NewDefault},
-		// Go
-		gobinary.Name: {gobinary.NewDefault},
-		// Javascript
-		nodemodules.Name: {nodemodules.New},
-		// Rust
-		cargoauditable.Name: {cargoauditable.NewDefault},
+	"artifact": concat(
+		without(list.Artifact, []string{secrets.Name, packagejson.Name}),
+		list.InitMap{
+			nodemodules.Name: {nodemodules.New},
+		},
+	),
+}
 
-		// --- OS packages ---
-		// Alpine
-		apk.Name: {apk.NewDefault},
-		// Debian
-		dpkg.Name: {dpkg.NewDefault},
-	},
+func without(initMap list.InitMap, omit []string) list.InitMap {
+	result := list.InitMap{}
+
+	maps.Copy(result, initMap)
+
+	for _, name := range omit {
+		delete(result, name)
+	}
+
+	return result
+}
+
+func concat(initMaps ...list.InitMap) list.InitMap {
+	result := list.InitMap{}
+	for _, m := range initMaps {
+		maps.Copy(result, m)
+	}
+
+	return result
 }
 
 func ResolveEnabledExtractors(enabledExtractors []string, disabledExtractors []string) []filesystem.Extractor {
