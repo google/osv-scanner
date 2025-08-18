e06e67df3c8a5585711cf3d3e17f1572f8108b33
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-18 16:38:50 +1200

    test: copy `.gitignore` files as part of setup (#2190)
    This overhauls our test logic for setting up git related testdata as a
    result of me getting confused by us having files ignored by git checked
    into the repository.
    
    The new `testcmd.setupGitFixtures` function handles initializing a git
    repository within `./fixtures` along with copying any `test.gitignore`
    files to `.gitignore`, returning a cleanup function for removing these
    to be called after the tests have run

--- cmd/osv-scanner/testmain_test.go
+++ cmd/osv-scanner/testmain_test.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"log/slog"
-	"os"
 	"testing"
 
-	"github.com/go-git/go-git/v5"
 	"github.com/google/osv-scanner/v2/cmd/osv-scanner/fix"
 	"github.com/google/osv-scanner/v2/cmd/osv-scanner/internal/cmd"
 	"github.com/google/osv-scanner/v2/cmd/osv-scanner/internal/testcmd"
@@ -16,13 +14,11 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	// ensure a git repository doesn't already exist in the fixtures directory,
-	// in case we didn't get a chance to clean-up properly in the last run
-	os.RemoveAll("./fixtures/.git")
+	cleanupGitFixtures, err := testcmd.SetupGitFixtures()
 
-	// Temporarily make the fixtures folder a git repository to prevent gitignore files messing with tests.
-	_, err := git.PlainInit("./fixtures", false)
 	if err != nil {
+		cleanupGitFixtures()
+
 		panic(err)
 	}
 
@@ -34,7 +30,7 @@ func TestMain(m *testing.M) {
 	}
 	m.Run()
 
-	os.RemoveAll("./fixtures/.git")
+	cleanupGitFixtures()
 
 	testutility.CleanSnapshots(m)
 }
