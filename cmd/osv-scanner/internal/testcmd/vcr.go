25565255358c9e18f59a060646bf903dc130d519
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-08-25 12:39:59 +1200

    test: sort cassette interactions based on their test name

--- cmd/osv-scanner/internal/testcmd/vcr.go
+++ cmd/osv-scanner/internal/testcmd/vcr.go
@@ -1,9 +1,13 @@
 package testcmd
 
 import (
+	"cmp"
+	"fmt"
+	"io/fs"
 	"net/http"
 	"os"
 	"path/filepath"
+	"slices"
 	"strings"
 	"testing"
 
@@ -105,4 +109,31 @@ func InsertCassette(t *testing.T) *http.Client {
 	})
 
 	return r.GetDefaultClient()
+}
+
+// SortCassetteInteractions walks over each cassette in testdata/cassettes and
+// sorts the interactions recorded in each based on the X-Test-Name header
+func SortCassetteInteractions() {
+	err := filepath.Walk("./testdata/cassettes", func(path string, info fs.FileInfo, err error) error {
+		if err != nil || info.IsDir() || !strings.HasSuffix(path, ".yaml") {
+			return err
+		}
+
+		cass, err := cassette.Load(strings.TrimSuffix(path, ".yaml"))
+		if err != nil {
+			return err
+		}
+
+		// we don't need to worry about the interaction ids as they get updated as part of saving
+		slices.SortFunc(cass.Interactions, func(a, b *cassette.Interaction) int {
+			return cmp.Compare(a.Request.Headers.Get("X-Test-Name"), b.Request.Headers.Get("X-Test-Name"))
+		})
+
+		return cass.Save()
+	})
+
+	if err != nil {
+		fmt.Printf("error sorting cassette interactions: %v\n", err)
+		os.Exit(1)
+	}
 }
