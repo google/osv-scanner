f9364258d9b9cd437c926631816ff9895570bf94
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-10-07 16:00:55 +1300

    test: use a different name for the default config file and ensure all vulns are ignored (#2254)
    This should make it a lot easier to author and maintain cmd tests
    without disappointing tools like scorecard and dependabot, as it means
    going forward we can just drop a `osv-scanner.toml` configured to ignore
    everything into any `testdata` fixture that has actual vulns without it
    impacting our tests.
    
    This is even more useful in the context of
    https://github.com/google/osv-scanner/pull/2216 which has us report
    ignored vulns that weren't found, which currently happens a lot in our
    test suite due to how artificial everything is 😅
    
    Previously we'd discussed making this read from an env variable which I
    think could be useful for others, but this starts by just making our
    currently-constant "config file name" string an internally public
    variable so that we can change it as part of the setup of our tests

--- cmd/osv-scanner/fix/testmain_test.go
+++ cmd/osv-scanner/fix/testmain_test.go
@@ -7,11 +7,14 @@ import (
 	"github.com/google/osv-scanner/v2/cmd/osv-scanner/fix"
 	"github.com/google/osv-scanner/v2/cmd/osv-scanner/internal/cmd"
 	"github.com/google/osv-scanner/v2/cmd/osv-scanner/internal/testcmd"
+	"github.com/google/osv-scanner/v2/internal/config"
 	"github.com/google/osv-scanner/v2/internal/testlogger"
 	"github.com/google/osv-scanner/v2/internal/testutility"
 )
 
 func TestMain(m *testing.M) {
+	config.OSVScannerConfigName = "osv-scanner-test.toml"
+
 	slog.SetDefault(slog.New(testlogger.New()))
 	testcmd.CommandsUnderTest = []cmd.CommandBuilder{fix.Command}
 	m.Run()
