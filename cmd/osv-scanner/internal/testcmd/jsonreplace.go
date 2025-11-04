0923de891b701b6aff98a506348440da361ff4e4
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2025-11-05 11:47:14 +1300

    test: rewrite json replacement logic to handle arrays better

--- cmd/osv-scanner/internal/testcmd/jsonreplace.go
+++ cmd/osv-scanner/internal/testcmd/jsonreplace.go
@@ -91,31 +91,58 @@ var (
 	}
 )
 
-// replaceJSONInput takes a gjson path and replaces all elements the path matches with the output of matcher
-func replaceJSONInput(t *testing.T, jsonInput, path string, matcher func(toReplace gjson.Result) any) string {
+func expandArrayPaths(t *testing.T, jsonInput string, path string) []string {
 	t.Helper()
 
-	pathArray := []string{}
+	// split on the first intermediate #, if present
+	pathToArray, restOfPath, hasArrayPlaceholder := strings.Cut(path, ".#.")
 
-	// If there are more than 2 #, sjson cannot replace them directly. Iterate out all individual entries
-	if strings.Contains(path, "#") {
-		// Get the path ending with #
-		// E.g. results.#.packages.#.vulnerabilities => results.#.packages.#
-		numOfEntriesPath := path[:strings.LastIndex(path, "#")+1]
-		// This returns a potentially nested array of array lengths
-		numOfEntries := gjson.Get(jsonInput, numOfEntriesPath)
-
-		// Use it to build up a list of concrete paths
-		buildSJSONPaths(t, &pathArray, path, numOfEntries)
-	} else {
-		pathArray = append(pathArray, path)
+	// if there is no intermediate placeholder, check for (and cut) a terminal one
+	if !hasArrayPlaceholder {
+		pathToArray, hasArrayPlaceholder = strings.CutSuffix(path, ".#")
 	}
+
+	// if there are no array placeholders in the path, just return it
+	if !hasArrayPlaceholder {
+		return []string{path}
+	}
+
+	r := gjson.Get(jsonInput, pathToArray)
+
+	// skip properties that are not arrays
+	if !r.IsArray() {
+		return []string{}
+	}
+
+	// if property exists and is actually an array, build out the path to each item
+	// within that array
+	paths := make([]string, 0, len(r.Array()))
+
+	for i := range r.Array() {
+		static := pathToArray + "." + strconv.Itoa(i)
+
+		if restOfPath != "" {
+			static += "." + restOfPath
+		}
+		paths = append(paths, expandArrayPaths(t, jsonInput, static)...)
+	}
+
+	return paths
+}
+
+// replaceJSONInput takes a gjson path and replaces all elements the path matches with the output of matcher
+func replaceJSONInput(t *testing.T, jsonInput string, path string, matcher func(toReplace gjson.Result) any) string {
+	t.Helper()
 
 	var err error
 	json := jsonInput
-	for _, pathElem := range pathArray {
+	for _, pathElem := range expandArrayPaths(t, jsonInput, path) {
 		res := gjson.Get(jsonInput, pathElem)
-		// TODO: Optimize with byte arrays instead
+
+		if !res.Exists() {
+			continue
+		}
+
 		json, err = sjson.SetOptions(json, pathElem, matcher(res), &sjson.Options{Optimistic: true})
 		if err != nil {
 			t.Fatalf("failed to set element")
@@ -123,30 +150,4 @@ func replaceJSONInput(t *testing.T, jsonInput, path string, matcher func(toRepla
 	}
 
 	return json
-}
-
-func buildSJSONPaths(t *testing.T, pathToBuild *[]string, path string, structure gjson.Result) {
-	t.Helper()
-
-	if structure.IsArray() {
-		// More nesting to go
-		for i, res := range structure.Array() {
-			buildSJSONPaths(
-				t,
-				pathToBuild,
-				// Replace the first # with actual index
-				strings.Replace(path, "#", strconv.Itoa(i), 1),
-				res,
-			)
-		}
-	} else {
-		// Otherwise assume it is a number
-		if strings.Count(path, "#") != 1 {
-			t.Fatalf("programmer error: there should only be 1 # left")
-		}
-		for i2 := range int(structure.Int()) {
-			newPath := strings.Replace(path, "#", strconv.Itoa(i2), 1)
-			*pathToBuild = append(*pathToBuild, newPath)
-		}
-	}
 }
