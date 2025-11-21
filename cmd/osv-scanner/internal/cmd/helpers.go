9b24da6a4fbb1de357ac30e0ccdfd3abc5feaf97
garethroper <gareth.roper@est.tech>
2025-11-21 12:49:23 +0000

    Initial commit for branch

--- cmd/osv-scanner/internal/cmd/helpers.go
+++ cmd/osv-scanner/internal/cmd/helpers.go
@@ -37,6 +37,10 @@ EXAMPLES:
 
 	For full usage details, please refer to the help command of each subcommand (e.g. {{.Name}} scan --help).
 
+	Alternatively, you can access the README or detailed documentation below:
+	README: https://github.com/google/osv-scanner/blob/main/README.md
+	Detailed Documentation: https://google.github.io/osv-scanner/
+
 VERSION:
 	{{.Version}}
 
