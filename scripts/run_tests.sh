9fc84925db454fc20bf570207924a6a3836ca4e4
Rex P <rexpan@google.com>
2025-11-25 15:49:32 +1100

    feat: Run the tests inside a container to avoid local files from affecting the outcome

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,4 +6,22 @@ if [ "$TEST_ACCEPTANCE" = true ]; then
     scripts/build_test_images.sh
 fi
 
-go test ./... -coverpkg=./... -coverprofile coverage.out
+if [ "$DOCKER_TEST" = true ]; then
+    echo "Building test image..."
+    docker build -f scripts/test_env.dockerfile -t osv-scanner-test .
+
+    echo "Running tests in container..."
+    docker run --rm \
+        -v "$(pwd):/src" \
+        -v "/var/run/docker.sock:/var/run/docker.sock" \
+        -v "${GOPATH:-$HOME/go}/pkg/mod:/go/pkg/mod" \
+        -e TEST_ACCEPTANCE="$TEST_ACCEPTANCE" \
+        -e UPDATE_SNAPS="$UPDATE_SNAPS" \
+        -e TEST_VCR_MODE="$TEST_VCR_MODE" \
+        osv-scanner-test \
+        sh -c "git config --global --add safe.directory /src && ./scripts/run_tests.sh \"\$@\"" -- "$@"
+    exit $?
+fi
+
+
+go test ./... -coverpkg=./... -coverprofile coverage.out "$@"
