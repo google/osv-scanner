3768ea866223728cca8ce0c6ce7f32a73584c647
Xueqin Cui <72771658+cuixq@users.noreply.github.com>
2026-01-30 10:51:45 +1100

    chore: update Go version to v1.25.6 (#2490)
    There are vulnerabilities reported for Go v1.25.5 and this PR updates Go
    version to v1.25.6; for base images in docker files, update them to
    alpine 3.23.

--- scripts/run_lints.sh
+++ scripts/run_lints.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-export GOTOOLCHAIN="${GOTOOLCHAIN:-go1.25.5}"
+export GOTOOLCHAIN="${GOTOOLCHAIN:-go1.25.6}"
 go run github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.6.1 run ./... "$@"
