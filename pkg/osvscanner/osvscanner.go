b446f03b66b2779978a1722781a3855058c838ec
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-02-19 15:18:09 +1300

    refactor: rename flag

--- pkg/osvscanner/osvscanner.go
+++ pkg/osvscanner/osvscanner.go
@@ -90,7 +90,7 @@ type ExperimentalScannerActions struct {
 	FlagDeprecatedPackages bool
 
 	// Update config file(s) to ignore all found vulnerabilities
-	UpdateConfigIgnoreVulns bool
+	UpdateConfigIgnores bool
 
 	// Allows specifying user agent
 	RequestUserAgent string
@@ -306,7 +306,7 @@ func finalizeScanResult(scanResult results.ScanResults, actions ScannerActions) 
 		vulnerabilityResults.LicenseSummary = buildLicenseSummary(&scanResult)
 	}
 
-	if actions.UpdateConfigIgnoreVulns {
+	if actions.UpdateConfigIgnores {
 		err := updateConfigs(&vulnerabilityResults, &scanResult.ConfigManager)
 
 		if err != nil {
