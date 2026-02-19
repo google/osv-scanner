c42e875c34e8574b21717dd299e75062797a4050
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-02-20 07:54:52 +1300

    fix: account for multiple files using the same config

--- cmd/osv-scanner/scan/source/command_test.go
+++ cmd/osv-scanner/scan/source/command_test.go
@@ -1782,6 +1782,7 @@ func TestCommand_UpdateConfigIgnores(t *testing.T) {
 				"composer.lock",
 				"custom-config.toml",
 				"osv-scanner-test.toml",
+				"Gemfile.lock",
 				"package-lock.json",
 				"nested-1/package-lock.json",
 				"nested-1/osv-scanner-test.toml",
