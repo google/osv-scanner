6af653bbd16b2b3800ad099fefff6988bd668a0a
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-02-26 15:16:23 +1300

    test: add a config case with a package override

--- cmd/osv-scanner/scan/source/command_test.go
+++ cmd/osv-scanner/scan/source/command_test.go
@@ -1857,6 +1857,7 @@ func TestCommand_UpdateConfigIgnores_All(t *testing.T) {
 				"custom-config.toml",
 				"nested-1/osv-scanner-test.toml",
 				"nested-2/osv-scanner-test.toml",
+				"nested-3/osv-scanner-test.toml",
 			} {
 				b, err := os.ReadFile(testDir + "/" + file)
 
@@ -1995,6 +1996,7 @@ func TestCommand_UpdateConfigIgnores_Unused(t *testing.T) {
 				"custom-config.toml",
 				"nested-1/osv-scanner-test.toml",
 				"nested-2/osv-scanner-test.toml",
+				"nested-3/osv-scanner-test.toml",
 			} {
 				b, err := os.ReadFile(testDir + "/" + file)
 
