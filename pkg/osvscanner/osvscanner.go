2e2b2c1c48c8e9212b66246c23b344c175a7dbd5
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-03-04 10:20:08 +1300

    refactor: stick with saving as config map is not holding pointers

--- pkg/osvscanner/osvscanner.go
+++ pkg/osvscanner/osvscanner.go
@@ -397,7 +397,11 @@ func finalizeScanResult(scanResult results.ScanResults, actions ScannerActions) 
 	//  - c: filtering removes vulns from results, so need to account for that
 	if actions.UpdateConfigIgnores == "all" {
 		// todo: add output about having ignored vulns
-		addVulnConfigIgnores(&vulnerabilityResults, &scanResult.ConfigManager)
+		err := addVulnConfigIgnoresAndSave(&vulnerabilityResults, &scanResult.ConfigManager)
+
+		if err != nil {
+			return models.VulnerabilityResults{}, err
+		}
 	}
 
 	filtered := filterResults(&vulnerabilityResults, &scanResult.ConfigManager, actions.ShowAllPackages)
@@ -411,11 +415,7 @@ func finalizeScanResult(scanResult results.ScanResults, actions ScannerActions) 
 
 	if actions.UpdateConfigIgnores == "unused" {
 		// todo: add output about having ignored vulns
-		removeAllUnusedConfigIgnores(&scanResult.ConfigManager)
-	}
-
-	if actions.UpdateConfigIgnores != "" && actions.UpdateConfigIgnores != "none" {
-		err := saveAllConfigs(&scanResult.ConfigManager)
+		err := removeAllUnusedConfigIgnoresAndSave(&scanResult.ConfigManager)
 
 		if err != nil {
 			return models.VulnerabilityResults{}, err
