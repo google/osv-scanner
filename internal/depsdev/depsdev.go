58d29086630fdee487a1dba6d776b56ad635e0dc
Xueqin Cui <cuixq@google.com>
2026-04-14 14:20:04 +1000

    depsdev

--- internal/depsdev/depsdev.go
+++ internal/depsdev/depsdev.go
@@ -2,9 +2,14 @@
 package depsdev
 
 import (
-	"github.com/ossf/osv-schema/bindings/go/osvconstants"
+	"crypto/x509"
+	"fmt"
 
-	depsdevpb "deps.dev/api/v3"
+	v3 "deps.dev/api/v3"
+	"deps.dev/api/v3alpha"
+	"github.com/ossf/osv-schema/bindings/go/osvconstants"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 // DepsdevAPI is the URL to the deps.dev API. It is documented at
@@ -12,12 +17,33 @@ import (
 const DepsdevAPI = "api.deps.dev:443"
 
 // System maps from a lockfile system to the depsdev API system.
-var System = map[osvconstants.Ecosystem]depsdevpb.System{
-	osvconstants.EcosystemNPM:      depsdevpb.System_NPM,
-	osvconstants.EcosystemNuGet:    depsdevpb.System_NUGET,
-	osvconstants.EcosystemCratesIO: depsdevpb.System_CARGO,
-	osvconstants.EcosystemGo:       depsdevpb.System_GO,
-	osvconstants.EcosystemMaven:    depsdevpb.System_MAVEN,
-	osvconstants.EcosystemPyPI:     depsdevpb.System_PYPI,
-	osvconstants.EcosystemRubyGems: depsdevpb.System_RUBYGEMS,
+var System = map[osvconstants.Ecosystem]v3.System{
+	osvconstants.EcosystemNPM:      v3.System_NPM,
+	osvconstants.EcosystemNuGet:    v3.System_NUGET,
+	osvconstants.EcosystemCratesIO: v3.System_CARGO,
+	osvconstants.EcosystemGo:       v3.System_GO,
+	osvconstants.EcosystemMaven:    v3.System_MAVEN,
+	osvconstants.EcosystemPyPI:     v3.System_PYPI,
+	osvconstants.EcosystemRubyGems: v3.System_RUBYGEMS,
+}
+
+// NewInsightsAlphaClient creates a deps.dev v3alpha InsightsClient with a custom address and userAgent.
+func NewInsightsAlphaClient(addr string, userAgent string) (v3alpha.InsightsClient, error) {
+	certPool, err := x509.SystemCertPool()
+	if err != nil {
+		return nil, fmt.Errorf("getting system cert pool: %w", err)
+	}
+	creds := credentials.NewClientTLSFromCert(certPool, "")
+	dialOpts := []grpc.DialOption{grpc.WithTransportCredentials(creds)}
+
+	if userAgent != "" {
+		dialOpts = append(dialOpts, grpc.WithUserAgent(userAgent))
+	}
+
+	conn, err := grpc.NewClient(addr, dialOpts...)
+	if err != nil {
+		return nil, fmt.Errorf("dialling %q: %w", addr, err)
+	}
+
+	return v3alpha.NewInsightsClient(conn), nil
 }
