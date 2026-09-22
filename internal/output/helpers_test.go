42d81efc5bdb45e3eb975c6d5a5c14d16d872a92
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-09-22 13:24:48 +1200

    test: use chars and repeat

--- internal/output/helpers_test.go
+++ internal/output/helpers_test.go
@@ -2,6 +2,7 @@ package output_test
 
 import (
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/google/osv-scalibr/extractor"
@@ -1326,8 +1327,10 @@ func testOutputWithVulnerabilities(t *testing.T, run outputTestRunner) {
 									Groups: []models.GroupInfo{{IDs: []string{"OSV-1"}}, {IDs: []string{"OSV-3"}}, {IDs: []string{"OSV-4"}}},
 									Vulnerabilities: []*osvschema.Vulnerability{
 										{Id: "OSV-1", Details: "This vulnerability allows for some very scary stuff to happen - seriously, you'd not believe it!"},
-										{Id: "OSV-3", Details: "界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界界"},
-										{Id: "OSV-4", Details: "界界界界界界界界界界界界界界界界界界界界界界界界界界 語語語語語語語語語語語語語語語語語語語語語語語語語語語語語語語語語語"},
+
+										// these ensure we're truncating multibyte characters properly
+										{Id: "OSV-3", Details: strings.Repeat("\u754c", 61)},
+										{Id: "OSV-4", Details: strings.Repeat("\u754c", 26) + " " + strings.Repeat("\u8a9e", 34)},
 									},
 								},
 								{
